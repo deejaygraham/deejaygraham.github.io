ed69703bb1360466a47953f347c70254d1ea955c
Derek Graham <deejaygraham@gmail.com>
2015-03-03 21:18:52 +0000

    Added copy for tags
    and refreshed tags.

--- build.sh
+++ build.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+# delete the tags folder
+rm -rfv ./tags/*
+
 jekyll build
+
+# create tags folder
+
+# copy built tags over 
+cp -r ./_site/tags/* ./tags/
+
 jekyll serve --baseurl= 
