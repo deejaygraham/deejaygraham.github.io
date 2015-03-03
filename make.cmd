d577551a2a21bd51c35906331eb4a8bda7e77b42
Derek Graham <deejaygraham@gmail.com>
2015-03-03 14:02:08 +0000

    Changed comment

--- make.cmd
+++ make.cmd
@@ -2,12 +2,12 @@
 
 SET MsBuildPath="%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
 
-rem Is this a 64 bit machine?
+:: Is this a 64 bit machine?
 IF EXIST "%Programfiles(x86)%" SET MsBuildPath="%windir%\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe"
 
 %MsBuildPath% /nologo BuildSite.msbuild
 
-rem Now run server
+:: Now run server
 jekyll serve --baseurl= 
 
 
