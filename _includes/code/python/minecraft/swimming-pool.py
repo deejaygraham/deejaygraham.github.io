8fdfbfbff20e76d2bd3c4646884b0d4af585ea11
Derek Graham <deejaygraham@gmail.com>
2018-05-10 19:57:30 +0000

    Create swimming-pool.py

---
+++ _includes/code/python/minecraft/swimming-pool.py
@@ -0,0 +1,31 @@
+import mcpi.minecraft as minecraft
+import mcpi.block as block
+
+world = minecraft.Minecraft.create()
+
+x, y, z = world.player.getTilePos()
+
+x += 2
+z += 2
+
+# clear area
+world.setBlocks(x, y, z, x + 20, y + 10, z + 20, block.AIR)
+
+width = 8
+height = 3
+depth = 5
+
+# glass block in the ground
+y -= height + 1
+world.setBlocks(x, y, z, x + depth, y + height, z + width, block.GLASS)
+
+# fill it with water
+x += 1
+z += 1
+y += 1
+
+depth -= 2
+width -= 2
+height -= 1
+world.setBlocks(x, y, z, x + depth, y + height, z + width, block.WATER)
+
