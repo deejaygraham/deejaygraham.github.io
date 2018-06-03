561739e9ca91de7a5a8b570d2c7bbbcd33d2a356
Derek Graham <deejaygraham@gmail.com>
2018-06-03 16:16:06 +0100

    mc refactor

--- _includes/code/python/minecraft/mc-controller.py
+++ _includes/code/python/minecraft/mc-controller.py
@@ -3,45 +3,46 @@ import mcpi.minecraft as minecraft
 import time
 
 world = minecraft.Minecraft.create()
+world.player.setting("autojump", True)
 
+# port on pi using ls /dev/tty*
 PORT = "/dev/ttyACM0"
 BAUD = 115200
-    
+
 microbit = serial.Serial(PORT)
 microbit.baudrate = BAUD
 microbit.parity   = serial.PARITY_NONE
 microbit.databits = serial.EIGHTBITS
 microbit.stopbits = serial.STOPBITS_ONE
 
+TILT = 100
+MIN_AXIS = -128
+MAX_AXIS = 128
+
 while True:
+  time.sleep(0.2)
+
   data = microbit.readline().decode('UTF-8')
   data_as_list = data.rstrip().split(' ')
-  
-  try:
-    # left-right, forwards-backwards, up-down  
-    controller_x, controller_y, controller_z, button_a, button_b = data_as_list
 
-    time.sleep(0.5)
-    position = world.player.getPos()
+  left_right, forwards_backwards, button_a, button_b = data_as_list
 
-    if controller_x > 0: position.x += 1
-    if controller_x < 0: position.x -= 1
-    #if controller_y > 0: position.z += 1
-    #if controller_y < 0: position.z -= 1
-    position.y = world.getHeight(position.x, position.z)
+  position = world.player.getTilePos()
 
-    # make sure we don't go too far one way or another.
-    
-    world.player.setPos(position.x, position.y, position.z)
-    #print(str(x) + " " + str(y) + " " + str(z) + " " + str(a) + " " + str(b))
-    
-    # print out the values ?
-    # do something with these values-------------------------------
-    # use x, y, z to move the player?
-	# if x > 0: move player right
-	# if x < 0: move player left
-	# world.player.setPos(x, y, z) ?
-  except:
-    pass
+  if left_right > TILT: position.x += 1
+  if left_right < -TILT: position.x -= 1
+  #if controller_y > 0: position.z += 1
+  #if controller_y < 0: position.z -= 1
 
-s.close()
+  # make sure we don't go too far one way or another.
+  position.x = min(max(position.x, MIN_AXIS), MAX_AXIS)
+  position.z = min(max(position.z, MIN_AXIS), MAX_AXIS)
+  position.y = world.getHeight(position.x, position.z)
+
+  # jump
+  if button_a:
+      position.y += 2
+
+  world.player.setTilePos(position.x, position.y, position.z)
+  # debug
+  # print(str(left_right) + " " + str(forwards_backwards) + " " + str(button_a) + " " + str(button_b))
