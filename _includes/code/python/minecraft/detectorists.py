c2b3d7471cc162615a826d1d96688ac54afb5595
Derek Graham <deejaygraham@gmail.com>
2018-07-12 13:28:03 +0100

    fixed tabs

--- _includes/code/python/minecraft/detectorists.py
+++ _includes/code/python/minecraft/detectorists.py
@@ -4,14 +4,14 @@ import time
 import random
 
 def random_underground_position():
-	return minecraft.Vec3(random.randint(-100, 100), random.randint(-20, 0), random.randint(-100, 100))
+  return minecraft.Vec3(random.randint(-100, 100), random.randint(-20, 0), random.randint(-100, 100))
 
 def distance_to_treasure(player, treasure):
-    dx = player.x - treasure.x
-    dy = player.y - treasure.y
-    dz = player.z - treasure.z
-    return math.ceil(math.sqrt((dx*dx) + (dy*dy) + (dz*dz)))
-	
+  dx = player.x - treasure.x
+  dy = player.y - treasure.y
+  dz = player.z - treasure.z
+  return math.ceil(math.sqrt((dx*dx) + (dy*dy) + (dz*dz)))
+  
 world = minecraft.Minecraft.create()
 
 treasure = random_underground_position()
@@ -29,20 +29,20 @@ while detectoring:
   position = world.player.getTilePos()
   
   if position != last_position:
-	distance = distance_to_treasure(playerTile, treasure)
+    distance = distance_to_treasure(playerTile, treasure)
   
-	if distance < 2:
-	  detectoring = False
-	elif distance < 10:
-	  world.postToChat("Hot")
-	else:
-	  if distance < last_distance:
-		world.postToChat("Warmer")
-	  elif distance > last_distance:
-		world.postToChat("Colder")
-	
-	last_distance = distance 
-	
+    if distance < 2:
+      detectoring = False
+    elif distance < 10:
+      world.postToChat("Hot")
+    else:
+      if distance < last_distance:
+        world.postToChat("Warmer")
+      elif distance > last_distance:
+        world.postToChat("Colder")
+
+    last_distance = distance 
+
   last_position = position
   time.sleep(2)
 
