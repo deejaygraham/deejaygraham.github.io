5d91661a1d2c319b151d02c149ed87169d050b20
Derek Graham <deejaygraham@gmail.com>
2018-07-12 12:58:28 +0100

    game

---
+++ _includes/code/python/minecraft/detectorists.py
@@ -0,0 +1,48 @@
+import mcpi.minecraft as minecraft
+import mcpi.block as block
+import time
+import random
+
+def random_underground_position():
+	return minecraft.Vec3(random.randint(-100, 100), random.randint(-20, 0), random.randint(-100, 100))
+
+def distance_to_treasure(player, treasure):
+    dx = player.x - treasure.x
+    dy = player.y - treasure.y
+    dz = player.z - treasure.z
+    return math.sqrt((dx*dx) + (dy*dy) + (dz*dz))
+	
+world = minecraft.Minecraft.create()
+
+treasure = random_underground_position()
+world.setBlock(treasure.x, treasure.y, treasure.z, block.GOLD)
+
+last_position = world.player.getTilePos()
+last_distance = distance_to_treasure(last_position, treasure)
+
+world.postToChat("Find the gold!")
+
+detectoring = True
+
+while detectoring:
+
+  position = world.player.getTilePos()
+  
+  if position != last_position:
+	distance = distance_to_treasure(playerTile, treasure)
+  
+	if distance < 2:
+	  detectoring = False
+	else:
+	  if distance < last_distance:
+		world.postToChat("Warmer")
+	  elif distance > last_distance:
+		world.postToChat("Colder")
+	
+	last_distance = distance 
+	
+  last_position = position
+  time.sleep(2)
+
+world.postToChat("You found the treasure")
+  
---
+++ _posts/2018-07-01-minecraft-detectorists.md
@@ -0,0 +1,22 @@
+---
+layout: post
+title: Minecraft Detectorists
+categories: [ code, minecraft ]
+published: true
+hero: minecraft 
+
+---
+
+Another simple minecraft game, this time using Pythagoras' theorem to work out distances between two points.
+
+We randomly bury a piece of treasure - a gold block, a diamond, whatever - then give the player clues about 
+how far away they are from it. The "game" ends when they dig up the block.
+
+```python
+
+{% include code/python/minecraft/detectorists.py %}
+
+```
+
+Inspired by code I originally saw at Martin O'Hanlon's excellent <a href="https://www.stuffaboutcode.com/2013/01/raspberry-pi-minecraft-hide-and-seek.html">Stuff 
+About Code</a>.
