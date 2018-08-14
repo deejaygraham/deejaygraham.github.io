bf8efff6bd5e837f8ec624a494d84af7ac1daa46
Derek Graham <deejaygraham@gmail.com>
2018-08-14 12:39:42 +0100

    fixed tabs

--- _includes/code/python/minecraft/tree-3.py
+++ _includes/code/python/minecraft/tree-3.py
@@ -4,11 +4,11 @@ for i in range(1, 10):
     # change some characteristics 
     tree_spacing = random.randint(8, 12)
     trunk_height = random.randint(3, 10)
-	trunk_diameter = random.randint(5)
+    trunk_diameter = random.randint(5)
     leaf_height = random.randint(3, 10)
     build_tree(world, 
-				x + (i * tree_spacing), 
-				z + (j * tree_spacing), 
-				trunk_height = trunk_height, 
-				trunk_diameter = trunk_diameter, 
-				leaf_height = leaf_height)
+        x + (i * tree_spacing), 
+        z + (j * tree_spacing), 
+        trunk_height = trunk_height, 
+        trunk_diameter = trunk_diameter, 
+        leaf_height = leaf_height)
