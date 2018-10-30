c5a43852f39e30644821244572d1e5741337a411
Derek Graham <deejaygraham@gmail.com>
2018-10-30 18:14:15 +0000

    ghost detector

---
+++ _includes/code/python/microbit/ghost-detector.py
@@ -0,0 +1,53 @@
+# ghost detector - looking for changes in temperature or magnetic field
+from microbit import *
+
+sleep_time = 200
+warning_time = 5000
+
+
+class Pinger:
+    
+  def __init__(self):
+    self.direction = 1
+    self.x = 2
+    self.y = 2
+     
+  def update(self):
+    display.set_pixel(self.x, self.y, 0)
+    if self.x >= 4: 
+        self.x = 4
+        self.direction = -self.direction
+    elif self.x <= 0:
+        self.x = 0
+        self.direction = -self.direction
+        
+    self.x += self.direction
+    display.set_pixel(self.x, self.y, 9)        
+
+ 
+class GhostDetector:
+   
+  def __init__(self):
+     self.reset()
+     
+  def ghost_detected(self):
+    now_temperature = temperature()
+    return self.starting_temperature != now_temperature
+
+  def reset(self):
+    self.starting_temperature = temperature()
+     
+     
+pinger = Pinger()
+detector = GhostDetector()
+
+while True:
+  
+  pinger.update()
+  sleep(sleep_time)
+      
+  if detector.ghost_detected():
+     detector.reset() 
+     display.show(Image.GHOST)
+     sleep(warning_time)
+     display.clear()
---
+++ _posts/2018-10-30-microbit-ghost-detector.md
@@ -0,0 +1,16 @@
+---
+layout: post
+title: Microbit Ghost Detector
+categories: [ code, microbit ]
+published: true
+hero: microbit
+---
+
+Just in time for Halloween, here's a ghost detector that uses the microbit temperature sensor to sense changes in ambient temperature - which we all know is a sure sign that there's a ghost somewhere nearby :)
+
+
+```python
+
+{% include code/python/microbit/ghost-detector.py %}
+
+```
