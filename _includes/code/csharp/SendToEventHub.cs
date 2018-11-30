e2a94fb7328593410abc10ba89ab69f66a42715f
Derek Graham <deejaygraham@gmail.com>
2018-11-30 09:39:26 +0000

    code tidy

--- _includes/code/csharp/SendToEventHub.cs
+++ _includes/code/csharp/SendToEventHub.cs
@@ -3,17 +3,24 @@ using Microsoft.ServiceBus.Messaging;
 
 ...
 
-// Connection data from the Azure portal
-string connectionString = "Endpoint=sb://my-environment.servicebus.windows.net/;SharedAccessKeyName=MyEventHub;SharedAccessKey=ThisIsNotAKey=";
-string eventHubName = "my-event-hub";
+// Connection data taken from the Azure portal
+const string endpoint = "sb://my-environment.servicebus.windows.net/";
+const string eventHubName = "my-event-hub";
+const string SAKName = "MyEventHub";
+const string SAKSecret = "ThisIsNotAKey=";
 
+string connectionString = string.Format("Endpoint={0};SharedAccessKeyName={1};SharedAccessKey={2}",
+									SAKName,
+									SAKSecret,
+									endpoint);
+									
 var client = EventHubClient.CreateFromConnectionString(connectionString, eventHubName);
 
-// send a single piece of data.
-string json = JsonConvert.SerializeObject(new ApplicationSpecificEvent
-											{
-												Value = thisIsMyValue,
-												Timestamp = DateTime.UtcNow
-											});
+var message = new ApplicationSpecificEvent
+{
+	Value = thisIsMyValue,
+	Timestamp = DateTime.UtcNow
+};
 
+string json = JsonConvert.SerializeObject(message);
 client.Send(new EventData(Encoding.Default.GetBytes(json)));
