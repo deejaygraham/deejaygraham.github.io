da2d327dbb8969b968decddbbe8efef1eb549c7d
Derek Graham <deejaygraham@gmail.com>
2019-04-11 14:37:32 +0100

    Update Lint-Scripts.ps1

--- _includes/code/powershell/Lint-Scripts.ps1
+++ _includes/code/powershell/Lint-Scripts.ps1
@@ -7,6 +7,6 @@ Get-ChildItem -Path $here -Filter *.ps1 -Recurse | ForEach-Object {
     Invoke-ScriptAnalyzer -Path $_.FullName -IncludeDefaultRules -OutVariable Failures
 
 	If ($Failures.Count -gt 0) {
-		Write-Output "$_.FullName - $($Failures.Count) rule failures"
+		Write-Output "$($_.Name) - $($Failures.Count) rule failures"
 	}
 }
