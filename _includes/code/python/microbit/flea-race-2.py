2e79330355678941a64cfa5ad4a0aff4ff44b5f4
Derek Graham <deejaygraham@gmail.com>
2019-05-17 14:07:34 +0100

    Update flea-race-2.py

--- _includes/code/python/microbit/flea-race-2.py
+++ _includes/code/python/microbit/flea-race-2.py
@@ -7,7 +7,7 @@ display.set_pixel(flea[0], flea[1], 9)
 
 for x in range(1, 4):
     # turn off current pixel
-    display.set_pixel(turtle[0], turtle[1], 0)
+    display.set_pixel(flea[0], flea[1], 0)
     # move
     flea[0] += 1
     display.set_pixel(flea[0], flea[1], 9)
