68cecc40c498fdffe7453919857ea5a81073a626
Derek Graham <deejaygraham@gmail.com>
2019-05-17 16:46:46 +0100

    motoring code

--- _includes/code/python/microbit/bitbot-motor-2.py
+++ _includes/code/python/microbit/bitbot-motor-2.py
@@ -1,10 +1,31 @@
 
-def BitBot:
-
-    def __init__(self):
-        self.left_motor = Motor()
-        self.right_motor = Motor()
+class BitBot:
     
-    def circle(self, left_speed, right_speed):
-        self.left_motor.forward(left_speed)
-        self.right_motor.forward(right_speed)
+    def __init__(self):
+        self.left_motor = MicrobitMotor(pin0, pin8)
+        self.right_motor = MicrobitMotor(pin1, pin12)
+        
+    def forward(self, percent):
+        display.show(Image.ARROW_N)
+        self.left_motor.forward(percent)
+        self.right_motor.forward(percent)
+
+    def reverse (self, percent):
+        display.show(Image.ARROW_S)
+        self.left_motor.reverse(percent)
+        self.right_motor.reverse(percent)
+        
+    def cirlce_left (self, percent):
+        display.show(Image.ARROW_W)
+        self.left_motor.reverse(percent)
+        self.right_motor.forward(percent)
+        
+    def circle_right (self, percent):
+        display.show(Image.ARROW_E)
+        self.left_motor.forward(percent)
+        self.right_motor.reverse(percent)
+
+    def stop(self):
+        display.show(Image.HEART)
+        self.left_motor.stop()
+        self.right_motor.stop()       
