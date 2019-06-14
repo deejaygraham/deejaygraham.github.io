8488239f9d0d6d80b6a65b9b3b02bd8d336b492c
Derek Graham <deejaygraham@gmail.com>
2019-06-14 11:16:29 +0100

    debugging

--- _includes/code/python/microbit/microbit-temp-sensor.py
+++ _includes/code/python/microbit/microbit-temp-sensor.py
@@ -5,12 +5,16 @@ brightness = 4
 while True:
 
     temp_in_c = temperature()
-
+    pixel_temp = 0
+    
     for x in range(5):
         for y in range(5):
-            temp_pixel = y * 8 + x
-            pixel_intensity = brightness if int(temp_in_c) >= temp_pixel else 0
-                
+            pixel_temp += 2
+            pixel_intensity = brightness if int(temp_in_c) >= pixel_temp else 0
             display.set_pixel(x, y, pixel_intensity)
 
+            # debug only
+            # print(str(x) + ', ' + str(y) + ' : ' + str(pixel_temp) + ' -> ' + str(temp_in_c))
+            # sleep(500)
+            
     sleep(500)
