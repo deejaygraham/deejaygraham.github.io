797c3c796c84850a4a36b9148dc7e6a2c9a92217
Derek Graham <deejaygraham@gmail.com>
2022-02-04 17:07:38 +0000

    rename file

--- _includes/code/python/microbit/binary-clock.py
+++ _includes/code/python/microbit/binary-clock.py
@@ -1,21 +1,24 @@
 from microbit import *
-import time
 
 # rjust does not exist in micropython
 def rjust(text, character, length):
     padding = character * (length - len(text))
     return padding + text
 
-def decimal_to_binary(value, length):
-    asBinary = bin(value).replace("0b", "")
-    return asBinary #rjust(asBinary, '0', length)
 
-def format_binary_for_screen(str):
-    return ':'.join(str[i:i + 5] for i in range(0, len(str), 5))
+def decimal_to_binary(decimal, length):
+    binary = bin(decimal).replace("0b", "")
+    return rjust(binary, "0", length)
 
-#reset_value = '11111:11111:11111:11111:11111'
+
+def format_binary_for_screen(binary):
+    formatted = ":".join(binary[i : i + 5] for i in range(0, len(binary), 5))
+    return formatted.replace("1", "9")
+
+
 # test
-reset_value = '00000:00000:00000:00000:11111'
+# reset_value = "00000:00000:00000:00000:99999"
+reset_value = "99999:99999:99999:99999:99999"
 
 value = 0
 binary = decimal_to_binary(value, 25)
@@ -24,8 +27,8 @@ formatted = format_binary_for_screen(binary)
 while True:
 
     binaryAsImage = Image(formatted)
-    display.Show(binaryAsImage)
-    #print(formatted)
+    display.show(binaryAsImage)
+    # print(formatted)
 
     if formatted == reset_value:
         value = 0
@@ -36,4 +39,3 @@ while True:
     formatted = format_binary_for_screen(binary)
 
     sleep(1000)
-
