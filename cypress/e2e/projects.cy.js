ba0d4806dea4d0b9323088ab75b3ef9835abcefd
Derek Graham <deejaygraham@gmail.com>
2024-04-04 17:20:22 +0100

    Update projects.cy.js

--- cypress/e2e/projects.cy.js
+++ cypress/e2e/projects.cy.js
@@ -2,6 +2,15 @@
 
 describe('projects page', () => {
 
+    beforeEach(() => {
+        cy.visit('/projects/');
+    });
  
+    it('contains correct title', () => {
+        cy.title()
+        .should('contain', ' - projects');
+
+        cy.get('h1')
+        .should('have.text', 'Things What I Have Made');
+    });
 });
-  
