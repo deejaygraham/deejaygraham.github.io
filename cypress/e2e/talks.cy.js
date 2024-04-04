22393cedc28f47af49a8061898216cc93ca63293
Derek Graham <deejaygraham@gmail.com>
2024-04-04 17:18:30 +0100

    Update talks.cy.js

--- cypress/e2e/talks.cy.js
+++ cypress/e2e/talks.cy.js
@@ -1,7 +1,21 @@
 /// <reference types="cypress" />
 
 describe('talks page', () => {
-
+    beforeEach(() => {
+        cy.visit('/talks/');
+    });
  
+    it('contains correct title', () => {
+        cy.title()
+        .should('contain', 'talks');
+    });
+
+    it('contains talk tiles', () => {
+        cy.get('.content')
+        .should('contain', 'Married to the Mob')
+        .should('contain', 'no SOLID evidence')
+        .should('contain', 'The Elements of Style')
+        .should('contain', 'Sketchnoting for Developers');
+    });
 });
   
