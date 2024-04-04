c3dbfcfa7ba0a864e0e33cf702ed51ee8896c857
Derek Graham <deejaygraham@gmail.com>
2024-04-04 17:29:35 +0100

    Update archive.cy.js

--- cypress/e2e/archive.cy.js
+++ cypress/e2e/archive.cy.js
@@ -2,6 +2,24 @@
 
 describe('archive page', () => {
 
+    beforeEach(() => {
+        cy.visit('/archive/');
+    });
  
+    it('contains correct title', () => {
+        cy.title()
+        .should('contain', ' - blog archive');
+
+        cy.get('h1')
+        .should('have.text', 'Blog Entries by Month and Year');
+    });
+
+    it('contains entries for each month and year', () => {
+        cy.get('.content')
+        .should('contain', 'March 2024')
+        .should('contain', 'Make A Jazz Noise Here')
+        .should('contain', 'November 2018')
+        .should('contain', 'Tracing Failed IIS Requests');
+    });
 });
   
