d8c8a69a2df2bdca971fee76f0a9b239bc231567
Derek Graham <deejaygraham@gmail.com>
2024-04-04 10:59:21 +0100

    Create blog.cy.js

---
+++ cypress/e2e/blog.cy.js
@@ -0,0 +1,43 @@
+/// <reference types="cypress" />
+
+describe('blog posts', () => {
+
+    context('slug formatting', () => {
+        it('contains one post from each year', () => {
+            cy.visit('/2011/12/18/why-i-am-an-agnostic/');
+            cy.visit('/2012/02/04/history/');
+            cy.visit('/2013/09/01/recursive-copy-in-msbuild/');
+            cy.visit('/2014/11/14/doctorows-three-laws/');
+            cy.visit('/2015/06/18/secret-azure-debugging-tools/');
+            cy.visit('/2016/09/23/balancing-ball-game-on-the-microbit/');
+            cy.visit('/2017/08/21/blinking-cpp-for-the-microbit/');
+            cy.visit('/2018/11/01/linting-in-powershell/');
+            cy.visit('/2019/09/17/slowing-down-selenium-tests/');
+            cy.visit('/2020/11/30/silent-night-for-the-microbit/');
+            cy.visit('/2021/08/20/check-file-content-with-powershell/');
+            cy.visit('/2022/09/30/improved-ghost-detector/');
+            cy.visit('/2023/09/10/run-csharp-from-powershell/');
+            cy.visit('/2024/03/28/make-a-jazz-noise-here/');
+        });
+    });
+
+    // code samples contain the correct markup
+    // tags 
+    // search bar
+    // other posts
+
+    // footer
+    // powered by
+    context('blog format', () => {
+
+        beforeEach(() => {
+            cy.visit('/2024/03/01/microbit-sound-meter/');
+        });
+
+        it('each post contains a date', () => {
+
+            //cy.visit('/2024/03/01/microbit-sound-meter/');
+        });
+    });
+});
+  
