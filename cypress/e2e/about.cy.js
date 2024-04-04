03db7ea2672ec67d0b1f0541f4c52ceb7c9acbcf
Derek Graham <deejaygraham@gmail.com>
2024-04-04 17:18:48 +0100

    Update about.cy.js

--- cypress/e2e/about.cy.js
+++ cypress/e2e/about.cy.js
@@ -2,6 +2,16 @@
 
 describe('about page', () => {
 
+    beforeEach(() => {
+        cy.visit('/about/');
+    });
  
+    it('contains correct title', () => {
+        cy.title()
+        .should('contain', 'about d.j.graham');
+
+        cy.get('h1')
+        .should('have.text', 'Hey, Hi, Hello.');
+    });
 });
   
