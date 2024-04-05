ca3e8d1df215522b89fe726822954acd51c492cf
Derek Graham <deejaygraham@gmail.com>
2024-04-05 09:37:27 +0100

    Update homepage.cy.js

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -1,1 +1,48 @@
+/// <reference types="cypress" />
 
+describe('home page', () => {
+    beforeEach(() => {
+      cy.visit('/')
+    });
+
+    it('contain home icon', () => {
+      cy.get('.navbar-brand > a')
+      .should('have.attr', 'href')
+      .and('equal', '/');
+    });
+  
+    it('navigation menu is shown', () => {
+      cy.get('.navbar-end > a')
+      .should('have.length', 6);
+
+      cy.get('.navbar-end > a')
+      .first()
+      .should('have.text', 'home');
+
+      cy.get('.navbar-end > a')
+      .last()
+      .should('have.text', 'about');
+    });
+
+    it('search form is shown', () => {
+
+      cy.get('form.navbar-item')
+      .should('have.attr', 'action')
+      .and('equal', 'https://deejaygraham.github.io/search/index.html');
+
+      cy.get('#search-button').should('have.text', 'Search');
+    });
+  
+    it('contains list of posts', () => {
+      cy.get('article')
+      .should('have.length', 36);
+    });
+  
+    it('contains footer', () => {
+      cy.get('.footer')
+      .scrollIntoView();
+
+      cy.get('.footer > nav-item').should('have.length', 3);
+    });
+  });
+  
