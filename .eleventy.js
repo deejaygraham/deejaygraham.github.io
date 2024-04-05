0a3166f2c6e4b82d142d6ff76ce4b6a17a1fdbef
Derek Graham <deejaygraham@gmail.com>
2024-04-05 20:46:49 +0100

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,19 @@
+// collections
+const posts = require("./src/_11ty/collections/posts.js");
 
+module.exports = function (eleventyConfig) {
+    
+  // collections
+  eleventyConfig.addCollection("posts", posts;
+
+  // ignores
+  eleventyConfig.ignores.add("src/assets/**/*");
+  eleventyConfig.watchIgnores.add("src/assets/**/*");
+
+  // passthrough copy
+  eleventyConfig.addPassthroughCopy("./src/assets/img/");
+  eleventyConfig.addPassthroughCopy("./src/assets/fonts/");
+    
 return {
     dir: {
       input: "src/content",
@@ -10,3 +25,4 @@ return {
     dataTemplateEngine: "njk",
     html: true
   };
+};
