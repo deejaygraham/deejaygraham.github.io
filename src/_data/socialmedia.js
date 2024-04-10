7285296c2e1e12200b043bc6240355d4fb3ffa23
Derek Graham <deejaygraham@gmail.com>
2024-04-10 10:37:25 +0100

    Create socialmedia.js

---
+++ src/_data/socialmedia.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    name: "RSS",
+    url: "/rss.xml",
+  },
+  {
+    name: "Github",
+    url: "https://github.com/deejaygraham",
+  },
+  {
+    name: "Mastodon",
+    url: "https://hachyderm.io/@deejaygraham",
+  },
+];
