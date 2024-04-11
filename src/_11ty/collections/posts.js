cb27b7d3187b0febf9447610123a25a6375b4904
Derek Graham <deejaygraham@gmail.com>
2024-04-11 11:21:22 +0100

    fix typo

--- src/_11ty/collections/posts.js
+++ src/_11ty/collections/posts.js
@@ -3,6 +3,6 @@ const now = new Date();
 module.exports = function (collection) {
   return collection
     .getFilteredByGlob("./src/content/posts/*.md")
-    ..filter((item) => item.data.draft !== true && item.date <= now)
+    .filter((item) => item.data.draft !== true && item.date <= now)
     .reverse();
 }
