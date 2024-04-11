142cd408a9fc01225a26cc3670fdd4a1d3add976
Derek Graham <deejaygraham@gmail.com>
2024-04-11 12:04:23 +0100

    Added missing filters

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,20 @@
 // collections
 const posts = require("./src/_11ty/collections/posts.js");
+const limit = require("./src/_11ty/filters/limit.js");
+const dates = require("./src/_11ty/filters/dates.js");
 
 module.exports = function (eleventyConfig) {
     
   // collections
   eleventyConfig.addCollection("posts", posts);
+
+  // filters
+  eleventyConfig.addFilter("limit", limit);
+  eleventyConfig.addFilter("dateISO", dates.dateISO);
+  eleventyConfig.addFilter("dateFeed", dates.dateFeed);
+  eleventyConfig.addFilter("dateFull", dates.dateFull);
+  eleventyConfig.addFilter("dateFormat", dates.dateFormat);
+  eleventyConfig.addFilter("dateYear", dates.dateYear);
 
   // ignores
   eleventyConfig.ignores.add("src/assets/**/*");
@@ -37,4 +47,4 @@ return {
     
     dataTemplateEngine: "njk",
   };
-};
+}
---
+++ src/_11ty/filters/dates.js
@@ -0,0 +1,73 @@
+const luxon = require("luxon");
+
+// thanks to jerome coupe
+
+/**
+ * Format date: Feeds
+ *
+ * @param {Date} date - JS date
+ * @returns {String} - formatted date
+ */
+function dateFeed(date) {
+  const jsDate = new Date(date);
+  const dt = luxon.DateTime.fromJSDate(jsDate);
+  return dt.toRFC2822();
+}
+
+/**
+ * Format date: Luxon format string
+ *
+ * @param {Date} date - JS date
+ * @param {String} format - Luxon format string
+ * @param {String} locale - locale code
+ * @returns {String} - formatted date
+ */
+function dateFormat(date, format, locale = "en") {
+  const jsDate = new Date(date);
+  const dt = luxon.DateTime.fromJSDate(jsDate);
+  return dt.setLocale(locale).toFormat(format);
+}
+
+/**
+ * Format date: human readable format
+ *
+ * @param {Date} date - JS date
+ * @param {String} locale - locale code
+ * @returns {String} - formatted date
+ */
+function dateFull(date, locale = "en") {
+  const jsDate = new Date(date);
+  const dt = luxon.DateTime.fromJSDate(jsDate);
+  return dt.setLocale(locale).toLocaleString(DateTime.DATE_FULL);
+}
+
+/**
+ * Format date: ISO
+ * @param {Date} date - JS Date
+ * @returns {String} - formatted date
+ */
+function dateISO(date) {
+  const jsDate = new Date(date);
+  const dt = luxon.DateTime.fromJSDate(jsDate);
+  return dt.toISO();
+}
+
+/**
+ * Format date: year
+ *
+ * @param {Date} date - js date
+ * @returns {String} - formatted date
+ */
+function dateYear(date) {
+  const jsDate = new Date(date);
+  const fullYear = jsDate.getFullYear();
+  return fullYear;
+}
+
+module.exports = { 
+    dateFeed, 
+    dateFormat, 
+    dateFull, 
+    dateISO, 
+    dateYear 
+};
