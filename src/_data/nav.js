06f108d31f6403f11768a7e82c56f10e975e4413
Derek Graham <deejaygraham@gmail.com>
2024-04-15 22:14:59 +0100

    Update nav.js

--- src/_data/nav.js
+++ src/_data/nav.js
@@ -1,32 +1,26 @@
 module.exports = [
   {
-    navLabel: "Home",
-    navTrigger: "home",
+    navLabel: "home",
     navUrl: "/",
   },
   {
     navLabel: "tags",
-    navTrigger: "tags",
     navUrl: "/tags",
   },
   {
     navLabel: "archive",
-    navTrigger: "archive",
     navUrl: "/archive",
   },
   {
     navLabel: "projects",
-    navTrigger: "projects",
     navUrl: "/projects",
   },
   {
     navLabel: "talks",
-    navTrigger: "talks",
     navUrl: "/talks",
   },
   {
     navLabel: "about",
-    navTrigger: "about",
     navUrl: "/about/",
   },
 ];
