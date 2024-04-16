8ac344ee0237cf4caa85026090688fbdb331c953
Derek Graham <deejaygraham@gmail.com>
2024-04-16 14:57:02 +0100

    Update socialmedia.js

--- src/_data/socialmedia.js
+++ src/_data/socialmedia.js
@@ -1,14 +1,40 @@
 module.exports = [
   {
-    name: "RSS",
-    url: "/rss.xml",
-  },
-  {
-    name: "Github",
+    name: "github",
     url: "https://github.com/deejaygraham",
+    fa: "fab fa-3x fa-github"
   },
   {
-    name: "Mastodon",
+    name: "mastodon",
     url: "https://hachyderm.io/@deejaygraham",
+  },
+  {
+    name: "bluesky",
+    url: "https://bsky.app/profile/deejaygraham.bsky.social",
+  },
+  {
+    name: "polywork",
+    url: "https://www.polywork.com/deejaygraham/",
+    fa: "fas fa-3x fa-draw-polygon"
+  },
+  {
+    name: "linkedin",
+    url: "https://www.linked.com/derekjohngraham/",
+    fa: "fab fa-3x fa-linkedin"
+  },
+  {
+    name: "slideshare",
+    url: "https://www.slideshare.net/deejaygraham",
+    fa: "fab fa-3x fa-slideshare"
+  },
+  {
+    name: "sessionize",
+    url: "https://sessionize.com/deejaygraham/",
+    fa: "fas fa-3x fa-chalkboard"
+  },
+  {
+    name: "rss",
+    url: "/rss.xml",
+    fa: "fa-rss"
   },
 ];
