dee00fa3ece6d326aa646dd59f3be34df9602ebc
Derek Graham <deejaygraham@gmail.com>
2024-04-16 10:15:46 +0100

    Update talks.js

--- src/_data/talks.js
+++ src/_data/talks.js
@@ -1,51 +1,52 @@
 module.exports = [
   {
-    title="no SOLID evidence"
-    subtitle="NE Remote Programming Conference"
-    url="https://www.slideshare.net/deejaygraham/no-solid-evidence"
-    thumbnail="/img/posts/talks/no-solid-evidence.webp"
+    title:"no SOLID evidence",
+    subtitle:"NE Remote Programming Conference",
+    url:"https://www.slideshare.net/deejaygraham/no-solid-evidence",
+    thumbnail:"/img/posts/talks/no-solid-evidence.webp",
   },
   {
-    title="The Elements of Style 2"
-    subtitle="Sunderland Uni 2020"
-    url="https://www.slideshare.net/deejaygraham/the-elements-of-style-232564981"
-    thumbnail="/img/posts/talks/the-elements-of-style-2.webp"
+    title:"The Elements of Style 2",
+    subtitle:"Sunderland Uni 2020",
+    url:"https://www.slideshare.net/deejaygraham/the-elements-of-style-232564981",
+    thumbnail:"/img/posts/talks/the-elements-of-style-2.webp",
   },
   {
-    title="The Elements of Style"
-    url="https://www.slideshare.net/deejaygraham/the-elements-of-style-191457045"
-    thumbnail="/img/posts/talks/the-elements-of-style.webp"
+    title:"The Elements of Style",
+    url:"https://www.slideshare.net/deejaygraham/the-elements-of-style-191457045",
+    thumbnail:"/img/posts/talks/the-elements-of-style.webp",
   },
   {
-    title="Married to the Mob (programming)"
-    subtitle="DDD North 2019"
-    url="https://www.slideshare.net/deejaygraham/married-to-the-mob-programming-134117887"
-    thumbnail="/img/posts/talks/married-to-the-mob-programming.webp"
+    title:"Married to the Mob (programming)",
+    subtitle:"DDD North 2019",
+    url:"https://www.slideshare.net/deejaygraham/married-to-the-mob-programming-134117887",
+    thumbnail:"/img/posts/talks/married-to-the-mob-programming.webp",
   },
   {
-    title="Physical Computing"
-    url="https://www.slideshare.net/deejaygraham/physical-computing-91152232"
-    thumbnail="/img/posts/talks/physical-computing.webp"
+    title:"Physical Computing",
+    url:"https://www.slideshare.net/deejaygraham/physical-computing-91152232",
+    thumbnail:"/img/posts/talks/physical-computing.webp",
   },
   {
-    title="Married to the Mob (programming)"
-    subtitle="DDD North 2017" url="https://www.slideshare.net/deejaygraham/married-to-the-mob-programming"
-    thumbnail="/img/posts/talks/married-to-the-mob-programming.webp"
+    title="Married to the Mob (programming)",
+    subtitle:"DDD North 2017",
+    url:"https://www.slideshare.net/deejaygraham/married-to-the-mob-programming",
+    thumbnail:"/img/posts/talks/married-to-the-mob-programming.webp",
   },
   {
-    title="How Do I Unix?"
-    url="https://www.slideshare.net/deejaygraham/how-do-i-unix"
-    thumbnail="/img/posts/talks/how-do-i-unix.webp"
+    title:"How Do I Unix?",
+    url:"https://www.slideshare.net/deejaygraham/how-do-i-unix",
+    thumbnail:"/img/posts/talks/how-do-i-unix.webp",
   },
   {
-    title="Adventures in Azure Machine Learning"
-    url="https://www.slideshare.net/deejaygraham/adventures-in-azure-machine-learning-from-ne-bytes"
-    thumbnail="/img/posts/talks/adventures-in-azure-machine-learning.webp"
+    title:"Adventures in Azure Machine Learning",
+    url:"https://www.slideshare.net/deejaygraham/adventures-in-azure-machine-learning-from-ne-bytes",
+    thumbnail:"/img/posts/talks/adventures-in-azure-machine-learning.webp",
   },
   {
-    title="Sketchnoting for Developers"
-    subtitle="DDD North 2015"
-    url="https://www.slideshare.net/deejaygraham/sketchnoting-for-developers-at-ddd-north-2015"
-    thumbnail="/img/posts/talks/sketchnoting-for-developers.webp"
+    title:"Sketchnoting for Developers",
+    subtitle:"DDD North 2015",
+    url:"https://www.slideshare.net/deejaygraham/sketchnoting-for-developers-at-ddd-north-2015",
+    thumbnail:"/img/posts/talks/sketchnoting-for-developers.webp",
   }
 ]
