61d69a5e9ec9f1e85025ab57839f690adce4a518
Derek Graham <deejaygraham@gmail.com>
2024-04-19 16:01:59 +0100

    messing about with search

--- src/_11ty/filters/searchFilter.js
+++ src/_11ty/filters/searchFilter.js
@@ -4,12 +4,15 @@ const elasticlunr = require("elasticlunr");
 module.exports = function(collection) {
   var index = elasticlunr(function() {
     this.addField("title");
+    this.addField("tags");
+    this.setRef("id");
   });
 
   collection.forEach(page => {
     index.addDoc({
       id: page.url,
-      title: page.template.frontMatter.data.title
+      title: page.template.frontMatter.data.title,
+      tags: page.template.frontMatter.data.tags
     });
   });
 
--- src/assets/js/search.js
+++ src/assets/js/search.js
@@ -12,31 +12,39 @@
     });
 	  
     if (results) {
-      results.map((r) => {
-        const { id, title } = r.doc;
-        const el = document.createElement("li");
-        container.appendChild(el);
+      results.map((result) => {
 
-        const h3 = document.createElement("h3");
-        el.appendChild(h3);
-
-        const a = document.createElement("a");
-        a.setAttribute("href", id);
-        a.textContent = title;
-        h3.appendChild(a);
-
-        //const p = document.createElement("p");
-        //p.textContent = description;
-        //el.appendChild(p);
+        if (result.doc){
+          const { id, title } = result.doc;
+          const el = document.createElement("li");
+          container.appendChild(el);
+  
+          const h3 = document.createElement("h3");
+          el.appendChild(h3);
+  
+          const a = document.createElement("a");
+          a.setAttribute("href", id);
+          a.textContent = title;
+          h3.appendChild(a);
+  
+          //const p = document.createElement("p");
+          //p.textContent = description;
+          //el.appendChild(p);
+  
+        }
+        else {
+          const el = document.createElement("li");
+          container.appendChild(el);
+        }
       });
     } else {
-      container.innerHTML = "<p>No results found";
+      container.innerHTML = "<p>No results found</p>";
     }
   };
 
   fetch("/search-index.json").then((response) =>
-    response.json().then((rawIndex) => {
-      window.searchIndex = elasticlunr.Index.load(rawIndex);
+    response.json().then((jsonIndex) => {
+      window.searchIndex = elasticlunr.Index.load(jsonIndex);
       document.getElementById("search-box").addEventListener("input", search);
     })
   );
