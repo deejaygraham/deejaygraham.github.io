94e995e4ff86c254d9758ba427b782cab1c1be37
Derek Graham <deejaygraham@gmail.com>
2024-04-22 16:28:28 +0100

    Create poweredby.js

---
+++ src/_data/poweredby.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    name: "11ty",
+    url: "http://11ty.dev/"
+  },
+  {
+    name: "bulma css",
+    url: "https://bulma.io/"
+  },
+  {
+    name: "elasticlunr",
+    url: "http://elasticlunr.com/"
+  },
+  {
+    name: "prism syntax highlighting",
+    url: "https://prismjs.com/"
+  },
+];
