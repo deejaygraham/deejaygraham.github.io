6b9d583d77f983d64a3206b3faeff8e0a16ef7ed
Derek Graham <deejaygraham@gmail.com>
2024-04-24 12:09:04 +0100

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -22,7 +22,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("dateFormat", dates.dateFormat);
   eleventyConfig.addFilter("dateYear", dates.dateYear);
   eleventyConfig.addFilter("search", require("./src/_11ty/filters/searchFilter.js"));
-  eleventyConfig.addFilter("array_to_comma_string", require("./src/_11ty/filters/array_to_comma_string.js"));
+  eleventyConfig.addFilter("arrayToCommaString", require("./src/_11ty/filters/arrayToCommaString.js"));
     
   // Return all the tags used in a collection, including the 
   // 1tty tags. The list of tags is sorted into alphabetical order.
@@ -39,32 +39,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("filterTagList", function filterTagList(tags) {
     return (tags || []).filter(tag => ["all", "nav", "post", "posts"].indexOf(tag) === -1);
   });
-
-  // exclude a page from a collection so that we don't have self-referential data.
-  // must be a nunjucks filter to allow use of default page url.
-  /*eleventyConfig.addNunjucksFilter("excludeFromCollection", function(collection = [], pageUrl = this.ctx.page.url) {
-      return collection.filter(post => post.url !== pageUrl);
-  });
-*/
-  /*eleventyConfig.addFilter("filterByTags", function(collection = [], ...requiredTags) {
-      if(requiredTags) {
-          const flatTags = requiredTags.flat();
-          return collection.filter(post => {
-              return flatTags.every(tag => post.data.tags?.includes(tag));
-          });
-      }
-
-      return [];
-  });
-*/
-
-  //eleventyConfig.addNunjucksFilter("related", function(collection = []) {
-  //    const { tags: requiredTags, page } = this.ctx;
-  //    return collection.filter(post => {
-  //        return post.url !== page.url && requiredTags?.some(tag => post.data.tags?.includes(tag));
-  //    });
-  //});
-    
+  
   // ignores
   eleventyConfig.ignores.add("src/assets/**/*");
   eleventyConfig.watchIgnores.add("src/assets/**/*");
