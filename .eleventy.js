376f02da11721679015b6645285bb033fc1d01ec
Derek Graham <deejaygraham@gmail.com>
2024-04-24 12:13:07 +0100

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -23,22 +23,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("dateYear", dates.dateYear);
   eleventyConfig.addFilter("search", require("./src/_11ty/filters/searchFilter.js"));
   eleventyConfig.addFilter("arrayToCommaString", require("./src/_11ty/filters/arrayToCommaString.js"));
-    
-  // Return all the tags used in a collection, including the 
-  // 1tty tags. The list of tags is sorted into alphabetical order.
-  eleventyConfig.addFilter("getAllTags", collection => {
-    let tagSet = new Set();
-    for(let item of collection) {
-     (item.data.tags || []).forEach(tag => tagSet.add(tag));
-    }
-    return Array.from(tagSet).sort();
-  });
-
-  // filter a list of tags to remove 11ty in-built and retain the 
-  // tags explicitly added to posts.
-  eleventyConfig.addFilter("filterTagList", function filterTagList(tags) {
-    return (tags || []).filter(tag => ["all", "nav", "post", "posts"].indexOf(tag) === -1);
-  });
+  eleventyConfig.addFilter("getAllTags", require("./src/_11ty/filters/getAllTags.js"));
+  eleventyConfig.addFilter("filterTagList", require("./src/_11ty/filters/filterTagList.js"));
   
   // ignores
   eleventyConfig.ignores.add("src/assets/**/*");
