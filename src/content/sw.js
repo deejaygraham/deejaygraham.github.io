2294316be1cabac30c4378fcf4601478e94ed25d
Derek Graham <deejaygraham@gmail.com>
2024-05-31 09:32:55 +0100

    Update sw.js

--- src/content/sw.js
+++ src/content/sw.js
@@ -1,1 +1,13 @@
 // service worker code for site
+
+// change v1 to allow upgrade
+const addResourcesToCache = async (resources) => {
+  const cache = await caches.open('v1');
+  await cache.addAll(resources);
+};
+
+const putInCache = async (request, response) => {
+  const cache = await caches.open('v1');
+  await cache.put(request, response);
+};
+
