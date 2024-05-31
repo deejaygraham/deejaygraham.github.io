6a1b214047bb930b03b0d58c7774828c146690dc
Derek Graham <deejaygraham@gmail.com>
2024-05-31 09:36:35 +0100

    Update site.js

--- src/assets/js/site.js
+++ src/assets/js/site.js
@@ -1,10 +1,36 @@
 // The following code is based off a toggle menu by @Bradcomp
 // source: https://gist.github.com/Bradcomp/a9ef2ef322a8e8017443b626208999c1
-(function () {
+const addBulmaBurgerMenu = () => {
     var burger = document.querySelector('.navbar-burger');
     var menu = document.querySelector('#' + burger.dataset.target);
     burger.addEventListener('click', function () {
         burger.classList.toggle('is-active');
         menu.classList.toggle('is-active');
     });
-})();
+};
+
+
+const registerServiceWorker = async () => {
+  if ('serviceWorker' in navigator) {
+    try {
+      const registration = await navigator.serviceWorker.register(
+        'sw.js',
+        {
+          scope: './',
+        }
+      );
+      if (registration.installing) {
+        console.log('Service worker installing');
+      } else if (registration.waiting) {
+        console.log('Service worker installed');
+      } else if (registration.active) {
+        console.log('Service worker active');
+      }
+    } catch (error) {
+      console.error(`Registration failed with ${error}`);
+    }
+  }
+};
+
+//registerServiceWorker();
+addBulmaBurgerMenu();
