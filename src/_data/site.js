1fecf405cfdb6e77122b64b557f5d3aea43de0e2
Derek Graham <deejaygraham@gmail.com>
2024-09-26 10:54:55 +0100

    Update site.js

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,5 +1,5 @@
 module.exports = {
-  title: "invalid argument",
+  title: "invalid argument: a blog by d.j. graham",
   name: "d.j. graham",
   description: "a triumph of style over substance",
   url: /serve|watch/.test(process.argv.join()) ? "http://localhost:8080" : "https://deejaygraham.github.io",
