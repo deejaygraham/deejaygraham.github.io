3bc024918db979e8f340ce2c60c2ed5e6ec9673d
Derek Graham <deejaygraham@gmail.com>
2024-09-26 16:58:51 +0100

    Update search.cy.js

--- cypress/e2e/search.cy.js
+++ cypress/e2e/search.cy.js
@@ -1,76 +1,64 @@
 /// <reference types="cypress" />
 
-describe("search for an existing item", () => {
-  const searchTerm = "jazz";
-  const expectedPostTitle = "Make A Jazz Noise Here";
+describe("search the site", () => {
 
   beforeEach(() => {
-    cy.visit("/");
-    cy.get("#search-box").type(searchTerm);
-    cy.get("#search-button").click();
-  });
-
-  it("has a consistent page title", () => {
-    cy.get("h1.title").should("contain", "search");
-  });
-
-  it("search results show matching articles", () => {
-    cy.get("#search-results ul li")
-      .should("have.length", 1)
-      .first()
-      .should("have.text", expectedPostTitle);
-  });
-});
-
-describe("search for an post by quotation author", () => {
-  const searchTerm = "Lehman";
-  const expectedPostTitle = "Meir M Lehman";
-
-  beforeEach(() => {
-    cy.visit("/");
-    cy.get("#search-box").type(searchTerm);
-    cy.get("#search-button").click();
+    cy.visit("/search/");
   });
   
-  it("search results show matching articles", () => {
-    cy.get("#search-results ul li")
-      .should("have.length", 1)
-      .first()
-      .should("have.text", expectedPostTitle);
-  });
-});
-
-describe("search for a post by tag", () => {
-  const searchTerm = "build";
-  const expectedPostTitle = "Creating Builds in TFS 2015";
-
-  beforeEach(() => {
-    cy.visit("/");
-    cy.get("#search-box").type(searchTerm);
-    cy.get("#search-button").click();
-  });
-
-  it("search results show matching articles", () => {
-    cy.get("#search-results ul li")
-      .should("have.length", 1)
-      .first()
-      .should("have.text", expectedPostTitle);
-  });
-});
-
-describe("search for a not existing item", () => {
-  const searchTerm = "blargleargle";
-
-  before(() => {
-    cy.visit("/");
-    cy.get("#search-box").type(searchTerm);
-    cy.get("#search-button").click();
+  context("for a post by title", () => {
+    it("has a consistent page title", () => {
+      cy.get("h1.title").should("contain", "search");
+    });
+  
+    it("search results show matching articles", () => {
+      const searchTerm = "jazz";
+      const expectedPostTitle = "Make A Jazz Noise Here";
+      
+      cy.get("#search-box").type(searchTerm);
+      cy.get("#search-results ul li")
+        .should("have.length", 1)
+        .first()
+        .should("have.text", expectedPostTitle);
+    });
   });
 
-  it("search results not shown for nonsense term", () => {
-    cy.get("#search-results").should(
-      "have.text",
-      "Nothing to see here.",
-    );
+  context("for a post by quotation author", () => {
+    it("search results show matching articles", () => {
+      const searchTerm = "Lehman";
+      const expectedPostTitle = "Meir M Lehman";
+      
+      cy.get("#search-box").type(searchTerm);
+      cy.get("#search-results ul li")
+        .should("have.length", 1)
+        .first()
+        .should("have.text", expectedPostTitle);
+    });
+  });
+
+  context("for a post by tag", () => {
+    const searchTerm = "build";
+    const expectedPostTitle = "Creating Builds in TFS 2015";
+  
+    it("search results show matching articles", () => {
+      cy.get("#search-box").type(searchTerm);
+      cy.get("#search-results ul li")
+        .should("have.length", 1)
+        .first()
+        .should("have.text", expectedPostTitle);
+    });
+  });
+
+  context("for an unknown term", () => {
+  
+    it("search results not shown for nonsense term", () => {
+      const searchTerm = "blargleargle";
+      
+      cy.get("#search-box").type(searchTerm);
+      cy.get("#search-results").should(
+        "have.text",
+        "Nothing to see here.",
+      );
+    });
   });
 });
