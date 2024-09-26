22c3f44ef95c2dc060ad132a115117e53150f2cb
Derek Graham <deejaygraham@gmail.com>
2024-09-26 16:47:05 +0100

    Update unknown-page.cy.js

--- cypress/e2e/unknown-page.cy.js
+++ cypress/e2e/unknown-page.cy.js
@@ -5,7 +5,11 @@ describe("Try to load an unknown page", () => {
   const expectedPostTitle = "Make A Jazz Noise Here";
 
   it("shows a 404 page", () => {
-    cy.visit("/blargleargle/");
+    cy.visit({
+        method: "GET",
+        url: "/blargleargle/",
+        failOnStatusCode: false,
+    });
     cy.get("h1.title").should("have.text", "Content not found");
     cy.get("p").should("have.text", "Try going to the home page");
   });
