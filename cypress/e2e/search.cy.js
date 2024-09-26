07f5a5e8348778a40a3b58b259cfdd5641416a06
Derek Graham <deejaygraham@gmail.com>
2024-09-26 16:26:19 +0100

    Update search.cy.js

--- cypress/e2e/search.cy.js
+++ cypress/e2e/search.cy.js
@@ -22,6 +22,42 @@ describe("search for an existing item", () => {
   });
 });
 
+describe("search for an post by quotation author", () => {
+  const searchTerm = "Lehman";
+  const expectedPostTitle = "Meir M Lehman";
+
+  beforeEach(() => {
+    cy.visit("/");
+    cy.get("#search-box").type(searchTerm);
+    cy.get("#search-button").click();
+  });
+  
+  it("search results show matching articles", () => {
+    cy.get("#search-results ul li")
+      .should("have.length", 1)
+      .first()
+      .should("have.text", expectedPostTitle);
+  });
+});
+
+describe("search for a post by tag", () => {
+  const searchTerm = "build";
+  const expectedPostTitle = "Creating Builds in TFS 2015";
+
+  beforeEach(() => {
+    cy.visit("/");
+    cy.get("#search-box").type(searchTerm);
+    cy.get("#search-button").click();
+  });
+
+  it("search results show matching articles", () => {
+    cy.get("#search-results ul li")
+      .should("have.length", 1)
+      .first()
+      .should("have.text", expectedPostTitle);
+  });
+});
+
 describe("search for an not existing item", () => {
   const searchTerm = "blargleargle";
 
