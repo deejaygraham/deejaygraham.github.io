30b1df8103f66de129698c6d911aaf82359ccbe7
Derek Graham <deejaygraham@gmail.com>
2024-09-27 08:42:45 +0100

    Update homepage.cy.js

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -10,36 +10,17 @@ describe("home page", () => {
   });
 
   it("navigation menu is shown", () => {
-    cy.get(".navbar-end > a").should("have.length", 6);
-
+    cy.get(".navbar-end > a").should("have.length", 7);
     cy.get(".navbar-end > a").first().should("have.text", "home");
-
-    cy.get(".navbar-end > a").last().should("have.text", "about");
-  });
-
-  it("search form is shown", () => {
-    cy.get("form.navbar-item")
-      .should("have.attr", "action")
-      .and("equal", "https://deejaygraham.github.io/search/index.html");
-
-    cy.get("#search-button").should("have.text", "Search");
+    cy.get(".navbar-end > a").last().should("have.text", "now");
   });
 
   it("contains list of posts", () => {
-    cy.get("article").should("have.length", 36);
-
-    cy.get(".post-summary")
-      .first()
-      .then(($p) => {
-        cy.wrap($p).get("header > .tile-tags");
-        cy.wrap($p).get("header > .tile-thumbnail");
-        cy.wrap($p).get("header > .title");
-      });
+    cy.get("cell").should("have.length", 12);
   });
 
   it("contains footer", () => {
     cy.get(".footer").scrollIntoView();
-
-    cy.get(".footer .nav-item").should("have.length", 6);
+    cy.get(".level-item").should("have.length", 8);
   });
 });
