3f596db762f8f78e29fa9b1aafe706a13184c1a6
Derek Graham <deejaygraham@gmail.com>
2024-11-20 08:36:58 +0000

    Update arrayToCommaString.test.js

--- src/_11ty/filters/arrayToCommaString.test.js
+++ src/_11ty/filters/arrayToCommaString.test.js
@@ -1,14 +1,14 @@
-const test = require('ava');
-const arryToCommaString = require('./arrayToCommaString.js');
+import test from 'ava';
+import arrayToCommaString from './arrayToCommaString.js';
 
 test('empty array returns blank string', t => {
-    t.is(arryToCommaString([]), '');
+    t.is(arrayToCommaString([]), '');
 });
 
 test('single tag is not delimited', t => {
-    t.is(arryToCommaString(['code']), 'code');
+    t.is(arrayToCommaString(['code']), 'code');
 });
 
 test('multiple tags are delimited by commas', t => {
-    t.is(arryToCommaString(['code', 'microbit', 'python']), 'code,microbit,python');
+    t.is(arrayToCommaString(['code', 'microbit', 'python']), 'code,microbit,python');
 });
