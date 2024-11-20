a6fa9146d79c8263511c6957263d274136795a23
Derek Graham <deejaygraham@gmail.com>
2024-11-20 08:38:54 +0000

    Update getAllTags.js

--- src/_11ty/filters/getAllTags.js
+++ src/_11ty/filters/getAllTags.js
@@ -1,7 +1,7 @@
 // Return all the tags used in a collection, including the
 // 11ty tags.
 // The list of tags is sorted into alphabetical order.
-module.exports = function (collection) {
+export function getAllTags(collection) {
   let uniqueTags = new Set();
   for (let item of collection) {
     (item.data.tags || []).forEach((t) => uniqueTags.add(t));
