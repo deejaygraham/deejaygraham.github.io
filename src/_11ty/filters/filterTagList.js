4b3b0789d5a2374ae1fb7c57046248897dbd32fe
Derek Graham <deejaygraham@gmail.com>
2024-11-20 08:38:02 +0000

    Update filterTagList.js

--- src/_11ty/filters/filterTagList.js
+++ src/_11ty/filters/filterTagList.js
@@ -1,6 +1,6 @@
 // filter a list of tags to remove 11ty in-built and retain the
 // tags explicitly added to posts.
-module.exports = function (tags) {
+export function filterTagList(tags) {
   return (tags || []).filter(
     (tag) => ["all", "nav", "post", "posts"].indexOf(tag) === -1,
   );
