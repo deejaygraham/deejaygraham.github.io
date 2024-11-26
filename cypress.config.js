24ac069b2dedb43d4aaf0472695a98b5d7483eba
Derek Graham <deejaygraham@gmail.com>
2024-11-26 14:17:27 +0000

    Increase pageloadtimeout

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  pageLoadTimeout: 20_000,
   e2e: {
     baseUrl: "https://deejaygraham.github.io",
     viewportWidth: 1280,
