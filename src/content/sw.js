9f3e5558937eeaac21c1275516e7037aec29b4fe
Derek Graham <deejaygraham@gmail.com>
2024-12-11 14:38:56 +0000

    Update sw.js

--- src/content/sw.js
+++ src/content/sw.js
@@ -1,4 +1,5 @@
 // service worker code for site
+// based on the example https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
 
 // change v1 to allow upgrade
 const version = 'v1';
