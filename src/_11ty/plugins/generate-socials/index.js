50691ca9bb24867656b698485758bf573cfee625
Derek Graham <deejaygraham@gmail.com>
2024-12-16 09:14:30 +0000

    Remove unused plugin

--- src/_11ty/plugins/generate-socials/index.js
+++
@@ -1,9 +0,0 @@
-export default function(/*{ dir, results } */) {
-  console.log('generating socials');
-
-  //results.forEach((page) => {
-    //console.log(page);
-  //});
-
-  console.log('complete');
-}
