27af4e920bedb28fe0d3211c9da5255daf543778
Derek Graham <deejaygraham@gmail.com>
2025-01-08 08:49:06 +0000

    Visit homepage once and assert from there
    ...to fix timeouts happening just with github builds

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -1,25 +1,25 @@
 /// <reference types="cypress" />
 
 describe("home page", () => {
+  beforeEach(() => {
+      cy.visit("/");
+    });
+  
   it("contain home icon", () => {
-    cy.visit("/");
     cy.get(".navbar-brand > a").should("have.attr", "href").and("equal", "/");
   });
 
   it("navigation menu is shown", () => {
-    cy.visit("/");
     cy.get(".navbar-end > a").should("have.length", 7);
     cy.get(".navbar-end > a").first().should("have.text", "home");
     cy.get(".navbar-end > a").last().should("have.text", "now");
   });
 
   it("contains list of posts", () => {
-    cy.visit("/");
     cy.get(".cell").should("have.length", 12);
   });
 
   it("contains footer", () => {
-    cy.visit("/");
     cy.get(".footer").scrollIntoView();
     cy.get(".level-item").should("have.length", 8);
   });
