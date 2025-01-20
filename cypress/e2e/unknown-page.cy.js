f2b98011aec8a7322a8138af6e1325fe37d24632
Derek Graham <deejaygraham@gmail.com>
2025-01-20 11:03:32 +0000

    Update unknown-page.cy.js

--- cypress/e2e/unknown-page.cy.js
+++ cypress/e2e/unknown-page.cy.js
@@ -1,12 +1,16 @@
 /// <reference types="cypress" />
 
-describe("Try to load an unknown page", () => {
-  it("shows a 404 page", () => {
-    cy.visit({
-      method: "GET",
-      url: "/blargleargle/",
-      failOnStatusCode: false,
-    });
+describe("404 page", () => {
+  it("shows a 404 message when navigating to a non-existent page", () => {
+    cy.request(  
+      {
+        url: '/blargleargle/', 
+        failOnStatusCode: false
+      }).its('status').should('equal', 404);
+  });
+  
+  it("404 handler page exists", () => {
+    cy.visit('/404');
     cy.get("h1.title").should("have.text", "Erm...");
     cy.get("p").should("contain", "The page you were looking for does not exist");
   });
