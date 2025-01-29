4499427523ca9ca7ad822fa433a28b370d7ba732
Derek Graham <deejaygraham@gmail.com>
2025-01-29 13:17:13 +0000

    Delete cypress.config-local.js

--- cypress.config-local.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "cypress";
-
-export default defineConfig({
-  pageLoadTimeout: 60_000,
-  e2e: {
-    baseUrl: "http://localhost:4222",
-    viewportWidth: 1280,
-  },
-});
