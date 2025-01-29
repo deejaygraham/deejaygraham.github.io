28d8dea7b0727c0043014b077cdffcbf77c4132a
Derek Graham <deejaygraham@gmail.com>
2025-01-29 10:08:38 +0000

    Update aiBots.js

--- src/_data/aiBots.js
+++ src/_data/aiBots.js
@@ -1,3 +1,80 @@
-export default {
-
-}
+export default [
+  {
+    name: "CCBot",
+  },
+  {
+    name: "ChatGPT-User",
+  },
+  {
+    name: "GPTBot",
+  },
+  {
+    name: "Google-Extended",
+  },
+  {
+    name: "Google-CloudVertexBot",
+  },
+  {
+    name: "Applebot-Extended",
+  },
+  {
+    name: "anthropic-ai",
+  },
+  {
+    name: "ClaudeBot",
+  },
+  {
+    name: "Omgilibot",
+  },
+  {
+    name: "Omgili",
+  },
+  {
+    name: "FacebookBot",
+  },
+  {
+    name: "Diffbot",
+  },
+  {
+    name: "DuckAssistBot",
+  },
+  {
+    name: "AI2Bot",
+  },
+  {
+    name: "Bytespider",
+  },
+  {
+    name: "Kangaroo Bot",
+  },
+  {
+    name: "PanguBot",
+  },
+  {
+    name: "ImagesiftBot",
+  },
+  {
+    name: "PerplexityBot",
+  },
+  {
+    name: "cohere-ai",
+  },
+  {
+    name: "cohere-training-data-crawler",
+  },
+  {
+    name: "Meta-ExternalAgent",
+  },
+  {
+    name: "Meta-ExternalFetcher",
+  },
+  {
+    name: "Timpibot",
+  },
+  {
+    name: "Webzio-Extended",
+  },
+  {
+    name: "YouBot",
+  },
+];
