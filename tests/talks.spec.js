4976a9b83b7616c5b25a41567f29dfbf0a32127f
Derek Graham <deejaygraham@gmail.com>
2025-02-05 14:18:18 +0000

    Add files via upload

---
+++ tests/talks.spec.js
@@ -0,0 +1,19 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+test.describe("talks page", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/talks/");
+  });
+
+  test("contains correct title",async ({ page }) => {
+    await expect(page).toHaveTitle(/talks/);
+  });
+
+  test("contains talk tiles", async ({ page }) => {
+    await expect(page.getByRole('link', { name: 'Married to the Mob (programming) DDD North 2019' })).toBeVisible();
+    await expect(page.getByRole('link', { name: 'no SOLID evidence' })).toBeVisible();
+    await expect(page.getByRole('link', { name: 'The Elements of Style', exact: true })).toBeVisible();
+    await expect(page.getByRole('link', { name: 'Sketchnoting for Developers' })).toBeVisible();
+  });
+});
