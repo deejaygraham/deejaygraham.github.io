21de9e2087749dfb3d77e2092a2879a54e7c9b29
Derek Graham <deejaygraham@gmail.com>
2025-02-06 15:04:02 +0000

    Update external-links.spec.js

--- tests/external-links.spec.js
+++ tests/external-links.spec.js
@@ -7,13 +7,14 @@ test("GTD Sketchnote challenge is available", async ({ page }) => {
   await page.goto("/2015/02/15/sketchnote-challenge/");
   await expect(page).toHaveTitle(/Sketchnoting Challenge/);
 
-  await page.goto('/img/posts/sketchnoting-challenge/mac-power-users-hifi.png');
+  await checkResourceExists(page, '/img/posts/sketchnoting-challenge/mac-power-users-hifi.png');
 });
 
 test("Sketchnoting at DDD North 2015 is available", async ({ page }) => {
   await page.goto("/2015/10/26/sketchnoting-at-dddnorth-2015/");
   await expect(page).toHaveTitle(/Sketchnoting at DDDNorth 2015/);
-  await page.goto('/img/posts/sketchnoting-at-dddnorth-2015/luke-stringer.jpg');
+  
+  await checkResourceExists(page, '/img/posts/sketchnoting-at-dddnorth-2015/luke-stringer.jpg');
 });
 
 test("Sketchnoting for Developers talk is available", async ({ page }) => {
@@ -22,5 +23,14 @@ test("Sketchnoting for Developers talk is available", async ({ page }) => {
 });
 
 test("ACE Conference sketchnote link is available", async ({ page }) => {
-  await page.goto('/img/posts/sketchnotes-from-ace-2014/gilb.png');
+  await checkResourceExists(page, '/img/posts/sketchnotes-from-ace-2014/gilb.png');
 });
+
+/**
+ * @param {import('@playwright/test').Page} page
+ * @param {string} url
+ */
+async function checkResourceExists(page, url) {
+  const response = await page.request.get(url)
+  expect.soft(response.ok(), `${url} is not available`).toBeTruthy();
+}
