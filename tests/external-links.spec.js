60b5f3b5f3d6fe7ac930b730d3dc8957aff10645
Derek Graham <deejaygraham@gmail.com>
2025-02-06 14:43:48 +0000

    Create external-links.spec.js

---
+++ tests/external-links.spec.js
@@ -0,0 +1,26 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+// Externally linked Resources we want links to be correct and stable
+
+test("GTD Sketchnote challenge is available", async ({ page }) => {
+  await page.goto("/2015/02/15/sketchnote-challenge/");
+  await expect(page).toHaveTitle(/Sketchnoting Challenge/);
+
+  await page.goto('/img/posts/sketchnoting-challenge/mac-power-users-hifi.png');
+});
+
+test("Sketchnoting at DDD North 2015 is available", async ({ page }) => {
+  await page.goto("/2015/10/26/sketchnoting-at-dddnorth-2015/");
+  await expect(page).toHaveTitle(/Sketchnoting at DDDNorth 2015/);
+  await page.goto('/img/posts/sketchnoting-at-dddnorth-2015/luke-stringer.jpg');
+});
+
+test("Sketchnoting for Developers talk is available", async ({ page }) => {
+  await page.goto("/sketchnoting-for-developers");
+  await expect(page).toHaveTitle(/sketchnoting for developers/);
+});
+
+test("ACE Conference sketchnote link is available", async ({ page }) => {
+  await page.goto('/img/posts/sketchnotes-from-ace-2014/gilb.png');
+});
