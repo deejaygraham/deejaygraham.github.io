d2969252d0412d1e0e7d256700eaf453e38072f7
Derek Graham <deejaygraham@gmail.com>
2025-02-10 10:52:55 +0000

    Update code-examples.spec.js

--- tests/code-examples.spec.js
+++ tests/code-examples.spec.js
@@ -5,9 +5,6 @@ test.describe("code examples", () => {
   test("can be copied to clipboard", async ({ page }) => {
     await page.goto("/2024/11/01/microbit-sings-portal-theme/");
 
-    // chrome only
-    await page.context().grantPermissions(["clipboard-read", "clipboard-write"]);
-
     // click the copy to clip board 
     await page.getByRole('button').click();
 
