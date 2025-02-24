6c1d74142c2c9a4b385ff2c65426c2e3842b5ce9
Derek Graham <deejaygraham@gmail.com>
2025-02-24 12:54:29 +0000

    Update offline.cy.js

--- cypress/e2e/offline.cy.js
+++ cypress/e2e/offline.cy.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
 describe("offline page", () => {
-  it("404 handler page exists", () => {
+  it("offline page exists", () => {
     cy.visit('/offline.html');
     cy.get("h1.title").should("have.text", "Erm...");
     cy.get("p").should("contain", "no internet connection at the moment");
