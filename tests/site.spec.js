d9be616820d77a6dad97cd430929bd1a6732d49c
Derek Graham <deejaygraham@gmail.com>
2025-03-07 11:38:05 +0000

    Initial test of all pages link checking

---
+++ tests/site.spec.js
@@ -0,0 +1,24 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+import * as fs from 'fs';
+
+const readJSON = (filePath) => {
+  const jsonData = fs.readFileSync(filePath, 'utf-8');
+  return JSON.parse(jsonData);
+}
+
+test.describe("all pages", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/talks/");
+  });
+
+  test("check all links on each page", async ({ page }) => {
+    const data = readJSON('../_site/spider.json');
+    const urls = data.urls;
+
+    for (const url of urls) {
+      await page.goto(url);
+      
+      // find all the links on the page...
+  });
+});
