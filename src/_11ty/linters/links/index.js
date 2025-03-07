d6e9aa59085558d3925c47f3e100e8c7b641912d
Derek Graham <deejaygraham@gmail.com>
2025-03-07 10:42:52 +0000

    Create index.js

---
+++ src/_11ty/linters/links/index.js
@@ -0,0 +1,12 @@
+/**
+ * Linter for basic page content
+ * @param {*} content 
+ * @param {*} inputPath 
+ * @param {*} outputPath 
+ */
+module.exports = function(content, inputPath, outputPath) {
+  // https://www.11ty.dev/docs/config/#linters
+  if (!outputPath.endsWith(".html")) return;
+
+  
+};
