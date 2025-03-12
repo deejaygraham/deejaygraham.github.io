f5f6d479aa5c8adbd17894b801c7b5521877adf5
Derek Graham <deejaygraham@gmail.com>
2025-03-12 08:28:07 +0000

    Delete src/_11ty/filters/cacheBust.js

--- src/_11ty/filters/cacheBust.js
+++
@@ -1,7 +0,0 @@
-// usage: {{ /css/style.css | cacheBust }}
-export default function (url) {
-  const [urlPart, paramPart] = url.split("?");
-  const params = new URLSearchParams(paramPart || "");
-  params.set("v", DateTime.local().toFormat("X"));
-  return `${urlPart}?${params}`;
-}
