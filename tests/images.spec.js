ea708e323ea0d2162ebe1071dc5642eca0208ab0
Derek Graham <deejaygraham@gmail.com>
2025-03-17 09:55:01 +0000

    Update images.spec.js

--- tests/images.spec.js
+++ tests/images.spec.js
@@ -4,19 +4,16 @@ import { test, expect } from '@playwright/test';
 test.describe("images on random page", () => {
   test("sketchnote appears on page", async ({ page }) => {
     await page.goto("/2025/03/01/solving-the-rubiks-cube-in-20-seconds/");
-    const avatar = await page.getByAltText('a triumph of style over substance');
-
-    await expect(avatar).toBeVisible();
+    await expect(page.getByRole('img', { name: 'Solving the Rubik\'s Cube in' })).toBeVisible();
   });
 
   test("hero appears on page", async ({ page }) => {
     await page.goto("/about/");
-    await expect(page.getByRole('link', { name: 'home' })).toBeVisible();
+    await expect(page.getByRole('img', { name: 'Hey, Hi, Hello.' })).toBeVisible();
   });
 
   test("image appears in middle of post", async ({ page }) => {
     await page.goto("/2019/08/20/autonomous-microbit-vehicle/");
-    await expect(page.getByRole('link', { name: 'rss' })).toBeVisible();
-
+    await expect(page.getByRole('img', { name: 'straddling the line' })).toBeVisible();
   });
 });
