dd9cc2a58f1afbc6d51dcfe69bec3042c80afb79
Derek Graham <deejaygraham@gmail.com>
2025-03-21 15:54:10 +0000

    Update theme-switcher.js

--- src/_includes/scripts/theme-switcher.js
+++ src/_includes/scripts/theme-switcher.js
@@ -2,10 +2,10 @@ const themePreferenceKey = "theme-preference";
 const lightThemeValue = 'light';
 const darkThemeValue = 'dark';
 
-/* eslint-disable-next-line */
 const setThemePreference = (themeName) => {
     const bulmaThemeAttrName = 'data-theme';
     const htmlElement = document.documentElement;
+    
     if (themeName === lightThemeValue || themeName === darkThemeValue) {
         htmlElement.setAttribute(bulmaThemeAttrName, themeName);
     } else {
@@ -14,34 +14,49 @@ const setThemePreference = (themeName) => {
     }
 }
 
+const saveThemePreference = (themeName) => {
+    if (themeName === lightThemeValue || themeName === darkThemeValue) {
+        localStorage.setItem(themePreferenceKey, themeName);
+    } else {
+        localStorage.removeItem(themePreferenceKey);
+    }
+}
+
+const onClick = (themeName) => {
+    setThemePreference(themeName);
+    saveThemePreference(themeName);
+}
+
+const getThemePreference = () => {
+  if (localStorage.getItem(themePreferenceKey)) {
+    return localStorage.getItem(themePreferenceKey);
+  } else {
+    return window.matchMedia('(prefers-color-scheme: dark)').matches ? darkThemeValue : lightThemeValue;
+  }
+}
+
 /* eslint-disable-next-line */
 const registerThemeSwitch = () => {
-    const lightThemeId = "light-theme-toggle";
-    const lightThemeButton = document.getElementById(lightThemeId);
-    lightThemeButton.addEventListener("click", () => {
-        setThemePreference(lightThemeValue);
-        localStorage.setItem(themePreferenceKey, lightThemeValue);
-    });
 
-    const darkThemeId = "dark-theme-toggle";
-    const darkThemeButton = document.getElementById(darkThemeId);
-    darkThemeButton.addEventListener("click", () => {
-        setThemePreference(darkThemeValue);
-        localStorage.setItem(themePreferenceKey, darkThemeValue);
-    });
-
-    const systemThemeId ="system-theme-toggle";
-    const systemThemeButton = document.getElementById(systemThemeId);
-    systemThemeButton.addEventListener("click", () => {
-        setThemePreference('');
-        localStorage.removeItem(themePreferenceKey);
-    });
-
-    const preference = localStorage.getItem(themePreferenceKey);
+    const preference = getThemePreference();
     if (preference) {
         if (preference === darkThemeValue || preference === lightThemeValue) {
             setThemePreference(preference);
         }
     } 
     // no preference - leave as system preference
+    
+    const lightThemeId = "light-theme-toggle";
+    const lightThemeButton = document.getElementById(lightThemeId);
+    lightThemeButton.setAttribute('aria-pressed', preference === lightThemeValue);
+    lightThemeButton.addEventListener("click", () => onClick(lightThemeValue));
+
+    const darkThemeId = "dark-theme-toggle";
+    document.getElementById(darkThemeId);
+    darkThemeButton.setAttribute('aria-pressed', preference === darkThemeValue);
+    darkThemeButton.addEventListener("click", () => onClick(darkThemeValue));
+
+    const systemThemeId ="system-theme-toggle";
+    const systemThemeButton = document.getElementById(systemThemeId);
+    systemThemeButton.addEventListener("click", () => onClick(''));
 };
