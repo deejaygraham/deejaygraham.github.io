510cb15d49b69e1d22540742f9d9fba15483ea62
Derek Graham <deejaygraham@gmail.com>
2025-03-24 13:38:59 +0000

    Update projects.js

--- src/_data/projects.js
+++ src/_data/projects.js
@@ -9,18 +9,21 @@ export default [
   {
     data: {
       title: "Makers and Creators project resources",
+      subtitle: "Making computers fun for 8-18 year olds",
     },
     url: "https://deejaygraham.github.io/makers-n-creators/",
   },
   {
     data: {
       title: "Scrum Planning Poker",
+      subtitle: "Cards to use when estimating in the planning game2
     },
     url: "https://github.com/deejaygraham/PlanningPoker/",
   },
   {
     data: {
       title: "GitHub",
+      subtitle: "In progress repositories of various kinds",
     },
     url: "https://github.com/deejaygraham/",
   },
