a6f6c607ccb6598054a69a61e74c515b166cb69d
Derek Graham <deejaygraham@gmail.com>
2025-03-26 10:31:45 +0000

    Update tags.spec.js

--- tests/tags.spec.js
+++ tests/tags.spec.js
@@ -4,7 +4,7 @@ import { test, expect } from '@playwright/test';
 test("Tags page contains entries for each tagged topic", async ({ page }) => {
   await page.goto("/tags/");
 
-  await expect(page.getByText("#11ty")).toBeVisible();
-  await expect(page.getByText("#python")).toBeVisible();
-  await expect(page.getByText("#agile")).toBeVisible();
+  await expect(page.getByText("11ty")).toBeVisible();
+  await expect(page.getByText("python")).toBeVisible();
+  await expect(page.getByText("agile")).toBeVisible();
 });
