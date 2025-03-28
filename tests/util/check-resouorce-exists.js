bde9756ec1734b42adcf4aa175b5f1c9b26b8356
Derek Graham <deejaygraham@gmail.com>
2025-03-28 08:27:25 +0000

    Create check-resouorce-exists.js

---
+++ tests/util/check-resouorce-exists.js
@@ -0,0 +1,9 @@
+/**
+ * Check a web resource exists by making a request to it and checking the ok response. 
+ * @param {import('@playwright/test').Page} page
+ * @param {string} url
+ */
+export default async function (page, url) {
+  const response = await page.request.get(url)
+  expect.soft(response.ok(), `${url} is not available`).toBeTruthy();
+}
