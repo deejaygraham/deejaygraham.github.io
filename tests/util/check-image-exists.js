ee489883dd5365de9f253798a0ccd264e7f25c79
Derek Graham <deejaygraham@gmail.com>
2025-03-28 10:11:10 +0000

    Update check-image-exists.js

--- tests/util/check-image-exists.js
+++ tests/util/check-image-exists.js
@@ -3,5 +3,8 @@ import checkResourceExists from "./check-resource-exists.js";
 
 export default async function (page, imgTag) {
   const url = await imgTag.getAttribute("src");
-  checkResourceExists(page, url);
+
+  if (url) {
+    await checkResourceExists(page, url);
+  }
 }
