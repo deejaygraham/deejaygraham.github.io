5ca55abff224b13c7323323a81c2bcf297967c36
Derek Graham <deejaygraham@gmail.com>
2025-03-28 09:05:15 +0000

    Update images.spec.js

--- tests/images.spec.js
+++ tests/images.spec.js
@@ -1,5 +1,5 @@
 // @ts-check
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
 import checkImageExists from './util/check-image-exists.js';
 
 test.describe("images on random page", () => {
