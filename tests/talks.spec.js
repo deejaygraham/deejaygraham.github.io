1eeb6e523bfc983ae701dd736f6177a0dfa73087
Derek Graham <deejaygraham@gmail.com>
2025-03-28 11:57:07 +0000

    Update talks.spec.js

--- tests/talks.spec.js
+++ tests/talks.spec.js
@@ -13,7 +13,7 @@ test.describe("talks page", () => {
   test("contains talk tiles", async ({ page }) => {
     await expect(page.getByRole('link', { name: 'Married to the Mob (' }).first()).toBeVisible();
     await expect(page.getByRole('link', { name: 'no SOLID evidence' }).first()).toBeVisible();
-    await expect(page.getByRole('link', { name: 'The Elements of Style', exact: true })).toBeVisible();
-    await expect(page.getByRole('link', { name: 'Sketchnoting for Developers' })).toBeVisible();
+    await expect(page.getByRole('link', { name: 'The Elements of Style' }).first()).toBeVisible();
+    await expect(page.getByRole('link', { name: 'Sketchnoting for Developers' }).first()).toBeVisible();
   });
 });
