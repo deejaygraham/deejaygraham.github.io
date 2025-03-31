8a7f1322a1b393c5d81c70d3aaf7d6fb07fe83ee
Derek Graham <deejaygraham@gmail.com>
2025-03-31 11:19:47 +0100

    Create dates.test.js

---
+++ src/_11ty/filters/dates.test.js
@@ -0,0 +1,29 @@
+import test from "ava";
+import { dateRFC822, dateFormat, dateFull, dateISO, dateYear } from "./dates.js";
+
+const exampleDate = new Date(2025, 5, 19);
+
+// dateRFC822
+test("RFC 822 format displayed correctly", (t) => {
+  t.is(dateRFC822(exampleDate), "");
+});
+
+// dateFormat
+test("Formatted date displayed correctly", (t) => {
+  t.is(dateFormat(exampleDate, 'yyyy-MM-dd'), "");
+});
+
+// dateFull
+test("Full date is human readable", (t) => {
+  t.is(dateFull(exampleDate), "");
+});
+
+// dateISO
+test("ISO date is displayed correctly", (t) => {
+  t.is(dateISO(exampleDate), "");
+});
+
+// dateYear
+test("Year date is displayed correctly", (t) => {
+  t.is(dateYear(exampleDate), "");
+});
