32b36d8bab36e136c6a15807eb52a58eaf528227
Derek Graham <deejaygraham@gmail.com>
2025-03-31 14:02:53 +0100

    Update readingTime.test.js

--- src/_11ty/filters/readingTime.test.js
+++ src/_11ty/filters/readingTime.test.js
@@ -2,5 +2,11 @@ import test from "ava";
 import readingTime from "./readingTime.js";
 
 test("reading time for short sentence is less than a minute", (t) => {
-  t.is(readingTime('this is very short), /less than 1 minute/);
+  t.is(readingTime('this is very short'), /less than 1 minute/);
+});
+
+test("reading time for medium sentence is one minute", (t) => {
+  const sentence = 'this is a slightly longer sentence but read by someone who reads slowly';
+  const speed = sentence.length;
+  t.is(readingTime(sentence, speed), /about 1 minute/);
 });
