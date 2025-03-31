47857d37faa9138239d4c61a12318c827d2b9b7d
Derek Graham <deejaygraham@gmail.com>
2025-03-31 14:26:41 +0100

    Update readingTime.test.js

--- src/_11ty/filters/readingTime.test.js
+++ src/_11ty/filters/readingTime.test.js
@@ -2,17 +2,27 @@ import test from "ava";
 import readingTime from "./readingTime.js";
 
 test("reading time for short sentence is less than a minute", (t) => {
-  t.is(readingTime('this is very short'), /less than 1 minute/);
+  const sentence = 'this is very short';
+  const estimate = readingTime(sentence);
+  const regex = /less than 1 minute/;
+  
+  t.truthy(regex.test(estimate), `${estimate} does not match ${regex}`);
 });
 
 test("reading time for medium sentence is one minute", (t) => {
   const sentence = 'this is a slightly longer sentence but read by someone who reads slowly';
   const speed = sentence.length;
-  t.is(readingTime(sentence, speed), /about 1 minute/);
+  const estimate = readingTime(sentence, speed);
+  const regex = /about 1 minute/;
+  
+  t.truthy(regex.test(estimate), `${estimate} does not match ${regex}`);
 });
 
 test("reading time for longer sentence is more than 1 minute", (t) => {
   const sentence = 'this is a much longer sentence containing words and also read by someone who reads slowly';
   const speed = sentence.length / 2;
-  t.is(readingTime(sentence, speed), /about 2 minutes/);
+  const estimate = readingTime(sentence, speed);
+  const regex = /about 2 minutes/;
+  
+   t.truthy(regex.test(estimate), `${estimate} does not match ${regex}`);
 });
