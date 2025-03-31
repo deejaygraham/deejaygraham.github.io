ca047ae468c62a9dd06ae595fe1a888c125dc616
Derek Graham <deejaygraham@gmail.com>
2025-03-31 12:28:39 +0100

    Update dates.test.js

--- src/_11ty/filters/dates.test.js
+++ src/_11ty/filters/dates.test.js
@@ -1,29 +1,29 @@
 import test from "ava";
-import { dateRFC2822, dateFormat, dateFull, dateISO, dateYear } from "./dates.js";
+import dates from "./dates.js";
 
 const exampleDate = new Date(2025, 5, 19);
 
 // dateRFC822
-test("RFC 822 format displayed correctly", (t) => {
-  t.is(dateRFC2822(exampleDate), "");
+test("RFC 822 format includes day of week, date and timestamp", (t) => {
+  t.is(dates.dateRFC2822(exampleDate), "Thu, 19 Jun 2025 00:00:00 +0100");
 });
 
 // dateFormat
 test("Formatted date displayed correctly", (t) => {
-  t.is(dateFormat(exampleDate, 'yyyy-MM-dd'), "");
+  t.is(dates.dateFormat(exampleDate, 'yyyy-MM-dd'), "2025-06-19");
 });
 
 // dateFull
-test("Full date is human readable", (t) => {
-  t.is(dateFull(exampleDate), "");
+test("Full date is human readable day month year only", (t) => {
+  t.is(dates.dateFull(exampleDate), "19 June 2025");
 });
 
 // dateISO
-test("ISO date is displayed correctly", (t) => {
-  t.is(dateISO(exampleDate), "");
+test("ISO date is displayed as year month day and timestamp", (t) => {
+  t.is(dates.dateISO(exampleDate), "2025-06-19T00:00:00.000+01:00");
 });
 
 // dateYear
-test("Year date is displayed correctly", (t) => {
-  t.is(dateYear(exampleDate), "");
+test("Year date is year as integer", (t) => {
+  t.is(dates.dateYear(exampleDate), 2025);
 });
