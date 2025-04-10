7c28630029fe8a574377e880d91d8b04e8e9cd30
Derek Graham <deejaygraham@gmail.com>
2025-04-10 21:49:08 +0100

    Shiki installed

--- src/_11ty/plugins/shiki-syntax-highlighter/index.js
+++ src/_11ty/plugins/shiki-syntax-highlighter/index.js
@@ -1,21 +1,39 @@
-import { createHighlighter } from 'shiki'
-
 // use shiki https://shiki.style as the syntax highlighter 
 export default function(eleventyConfig, options) {
   // empty call to notify 11ty that we use this feature
-  // eslint-disable-next-line no-empty-function
   eleventyConfig.amendLibrary('md', () => {});
 
   eleventyConfig.on('eleventy.before', async () => {
-    const highlighter = await createHighlighter(options);
+    const shiki = await import('shiki');
+
+    const highlighter = await shiki.createHighlighter({
+      themes: ['vitesse-dark'],
+      langs: [
+        'bat',
+        'cpp',
+        'csharp',
+        'elixir',
+        'js',
+        'json',
+        'ruby',
+        'powershell',
+        'python',
+        'rust',
+        'sql',
+        'vb',
+        'xml',
+        ],
+    });
     
     eleventyConfig.amendLibrary('md', (library) => {
       library.set({
         highlight: (code, language) => {
-          return highlighter.codeToHtml(code, { 
-              lang: language,
-              theme: options.theme
-            });
+
+          return highlighter.codeToHtml(code, {
+            lang: language,
+            theme: options.theme
+          });
+        }
       });
     });
   });
