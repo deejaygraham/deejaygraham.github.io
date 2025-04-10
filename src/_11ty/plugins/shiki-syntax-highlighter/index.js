a10b5f5a65e089f7428eb81e3a87c398f7d2f011
Derek Graham <deejaygraham@gmail.com>
2025-04-10 13:16:12 +0100

    Create index.js

---
+++ src/_11ty/plugins/shiki-syntax-highlighter/index.js
@@ -0,0 +1,21 @@
+import { createHighlighter } from 'shiki'
+
+// use shiki https://shiki.style as the syntax highlighter 
+export default function(eleventyConfig, options) {
+  // empty call to notify 11ty that we use this feature
+  // eslint-disable-next-line no-empty-function
+  eleventyConfig.amendLibrary('md', () => {});
+
+  eleventyConfig.on('eleventy.before', async () => {
+    const highlighter = await createHighlighter({
+      themes: ['nord'],
+      langs: ['javascript'],
+    });
+    
+    eleventyConfig.amendLibrary('md', (mdLib) =>
+      mdLib.set({
+        highlight: (code, lang) => highlighter.codeToHtml(code, { lang }),
+      })
+    );
+  });
+};
