d3ac2fa4fef24d5777fd9534b03b9c2f01b8c013
Derek Graham <deejaygraham@gmail.com>
2025-04-14 13:50:59 +0100

    Update about.spec.js

--- tests/about.spec.js
+++ tests/about.spec.js
@@ -7,5 +7,8 @@ test.describe("about page", () => {
 
     await expect(page).toHaveTitle(/Hey, Hi, Hello./);
     await expect(page.getByText("My name is Derek Graham and this is my personal blog.")).toBeVisible();
+
+    // banner image has to exist
+    await expect(page.getByAltText('Hey, Hi, Hello.')).toHaveAttribute('src', '/img/heroes/hero-makers-and-creators.webp');
   });
 });
