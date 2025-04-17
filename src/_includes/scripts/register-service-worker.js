d54adc6ce89e24821a1b3b9627e219fcf49bb645
Derek Graham <deejaygraham@gmail.com>
2025-04-17 10:58:33 +0100

    Update register-service-worker.js

--- src/_includes/scripts/register-service-worker.js
+++ src/_includes/scripts/register-service-worker.js
@@ -1,14 +1,12 @@
 /* eslint-disable-next-line */
 const registerServiceWorker = async () => {
   if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      try {
-        const registration = await navigator.serviceWorker.register('/sw.js', {
-            scope: '/',
-        });
-      } catch (error) {
-        console.error(`Registration failed with ${error}`);
-      }
-    });
+    try {
+      const registration = await navigator.serviceWorker.register('/sw.js', {
+          scope: '/',
+      });
+    } catch (error) {
+      console.error(`Registration failed with ${error}`);
+    }
   }
 };
