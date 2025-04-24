a1c425ef9ffb634274f60f9f348fd15204745735
Derek Graham <deejaygraham@gmail.com>
2025-04-24 08:54:20 +0100

    Update and rename talks.spec.js to speaking.spec.js

--- tests/speaking.spec.js
+++ tests/speaking.spec.js
@@ -2,7 +2,7 @@
 import { test, expect } from '@playwright/test';
 import checkPageLinksExist from './util/check-page-links-exist.js';
 
-test.describe("talks page", () => {
+test.describe("speaking page", () => {
   test.beforeEach(async ({ page }) => {
     await page.goto("/talks/");
   });
@@ -12,10 +12,10 @@ test.describe("talks page", () => {
   });
 
   test("contains talk tiles", async ({ page }) => {
-    await expect(page.getByRole('link', { name: 'Married to the Mob (' }).first()).toBeVisible();
-    await expect(page.getByRole('link', { name: 'no SOLID evidence' }).first()).toBeVisible();
-    await expect(page.getByRole('link', { name: 'The Elements of Style' }).first()).toBeVisible();
-    await expect(page.getByRole('link', { name: 'Sketchnoting for Developers' }).first()).toBeVisible();
+    await expect(page.getByRole('link', { name: 'Married to the Mob (' }).first()).toBeTruthy();
+    await expect(page.getByRole('link', { name: 'no SOLID evidence' }).first()).toBeTruthy();
+    await expect(page.getByRole('link', { name: 'The Elements of Style' }).first()).toBeTruthy();
+    await expect(page.getByRole('link', { name: 'Sketchnoting for Developers' }).first()).toBeTruthy();
   });
 
   test('Page links are correct', async ({page}) => {
