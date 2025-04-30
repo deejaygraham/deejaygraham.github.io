419441a45dcb5cc11c766eefbd898bf406c6af62
Derek Graham <deejaygraham@gmail.com>
2025-04-30 14:29:36 +0100

    Update a11y.spec.js

--- tests/a11y/a11y.spec.js
+++ tests/a11y/a11y.spec.js
@@ -2,21 +2,23 @@
 import { test, expect } from '@playwright/test';
 import AxeBuilder from '@axe-core/playwright'; 
 
-[
-  '/',
-  '/search/',
-  '/tags/',
-  '/projects/',
-  '/talks/',
-  '/about/',
-  '/2025/03/25/efficiency/', //  quote example
-  '/2025/04/11/microbit-haiku/', // code example
-  '/2025/03/15/hexagonal-rails/', // sketchnote example
-].forEach((url) => {
-  test(`Checking ${url} as no accessibility problems`, async ({ page }) => {
-    await page.goto(url);
-   
-    const accessibilityScanResults = await new AxeBuilder({ page }).analyze(); 
-    expect(accessibilityScanResults.violations).toEqual([]);
+test.describe('run accessibility tests', { tag: '@a11y' }, () => {
+  [
+    '/',
+    '/search/',
+    '/tags/',
+    '/projects/',
+    '/talks/',
+    '/about/',
+    '/2025/03/25/efficiency/', //  quote example
+    '/2025/04/11/microbit-haiku/', // code example
+    '/2025/03/15/hexagonal-rails/', // sketchnote example
+  ].forEach((url) => {
+    test(`Checking ${url} as no accessibility problems`, async ({ page }) => {
+      await page.goto(url);
+     
+      const accessibilityScanResults = await new AxeBuilder({ page }).analyze(); 
+      expect(accessibilityScanResults.violations).toEqual([]);
+    });
   });
 });
