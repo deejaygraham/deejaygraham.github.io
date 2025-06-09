d3b54dd8e48322ae2fd0cbb00a0750fd560bf8c3
Derek Graham <deejaygraham@gmail.com>
2025-06-09 10:05:50 +0100

    Update projects.js

--- src/_data/projects.js
+++ src/_data/projects.js
@@ -8,6 +8,13 @@ export default [
   },
   {
     data: {
+      title: "Simple Microbit Assert",
+      subtitle: "Minimal python unit test assertion library for the microbit"
+    },
+    url: "https://github.com/deejaygraham/simple-microbit-assert"
+  },
+  {
+    data: {
       title: "Makers and Creators project resources",
       subtitle: "Making computers fun for 8-18 year olds",
     },
@@ -19,6 +26,13 @@ export default [
       subtitle: "Cards to use when estimating in the planning game",
     },
     url: "https://github.com/deejaygraham/PlanningPoker/",
+  },
+  {
+    data: {
+        title: "littleTiger",
+        subtitle: "My first ever personal site when I was first learning about html and css via dialup"
+    },
+    url: "https://github.com/deejaygraham/littleTiger"
   },
   {
     data: {
