6d6a002bc859150f177ccb1696fb6db96246bed9
Derek Graham <deejaygraham@gmail.com>
2025-06-12 09:31:23 +0100

    Update binary-clock.py

--- src/_includes/code/python/microbit/binary-clock.py
+++ src/_includes/code/python/microbit/binary-clock.py
@@ -15,27 +15,36 @@ def format_binary_for_screen(binary):
     formatted = ":".join(binary[i : i + 5] for i in range(0, len(binary), 5))
     return formatted.replace("1", "9")
 
+bits = 25
+minimum_bits = 2
+maximum_bits = 25
 
-# test
-# reset_value = "00000:00000:00000:00000:99999"
-reset_value = "99999:99999:99999:99999:99999"
-
-value = 0
-binary = decimal_to_binary(value, 25)
-formatted = format_binary_for_screen(binary)
+ticks = 0
+tick_frequency = 500
 
 while True:
+    binary = decimal_to_binary(ticks, maximum_bits)
+    formatted = format_binary_for_screen(binary)
 
     binaryAsImage = Image(formatted)
     display.show(binaryAsImage)
-    # print(formatted)
+    print(formatted)
 
-    if formatted == reset_value:
-        value = 0
-    else:
-        value += 1
+    sleep(tick_frequency)
 
-    binary = decimal_to_binary(value, 25)
-    formatted = format_binary_for_screen(binary)
-
-    sleep(1000)
+    ticks += 1
+    if ticks >= (2 ** bits):
+        ticks = 0
+        
+    if accelerometer.was_gesture("shake"):
+        ticks = 0
+        
+    if button_a.was_pressed():
+        bits = max(minimum_bits, bits - 1)
+        display.scroll(str(bits))
+        ticks = 0
+        
+    if button_b.was_pressed():
+        bits = min(bits + 1, maximum_bits)
+        display.scroll(str(bits))
+        ticks = 0
