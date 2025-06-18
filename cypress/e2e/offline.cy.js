494a10a087fcf0a90f983a5519e08f89b8c3d033
Derek Graham <deejaygraham@gmail.com>
2025-06-18 09:44:52 +0100

    Delete cypress directory

--- cypress/e2e/offline.cy.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="cypress" />
-
-describe("offline page", () => {
-  it("offline page exists", () => {
-    cy.visit('/offline.html');
-    cy.get("h1.title").should("have.text", "Erm...");
-    cy.get("p").should("contain", "no internet connection at the moment");
-  });
-});
