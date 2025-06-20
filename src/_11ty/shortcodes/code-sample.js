de647fbcb7d7adad9711b8bc6778eb9097dcb2c0
Derek Graham <deejaygraham@gmail.com>
2025-06-20 13:37:17 +0100

    Update code-sample.js

--- src/_11ty/shortcodes/code-sample.js
+++ src/_11ty/shortcodes/code-sample.js
@@ -2,18 +2,25 @@
 import * as prettier from "prettier";
 import * as fs from 'fs';
 
-export default function (language, filename) {
-  const backticks = "```";
-  const content = fs.readFileSync(filename, function(err, contents) {
-      if (err) return err;
-      return contents;
-  });
-                                  
-  const prettified = prettier.format(content, {
-    parser: language,
-    printWidth: 512,
-    tabWidth: 2
-  });
-                                     
-  return `${backticks} ${language}\n ${prettified}\n${backticks}`;
+export default async function (language, filename) {
+  		try {
+			  // Check if input file exists first
+			  if (!fs.existsSync(filename)) {
+				  console.error(`Source code file not found: ${filename}`);
+				  return '';
+			  }
+
+			const codeContent = await fs.promises.readFile(filename, 'utf8');
+      const prettyCode = await prettier.format(codeContent, {
+        parser: "babel",
+        printWidth: 512,
+        tabWidth: 2
+      });
+    
+      const backticks = "```";
+      return `${backticks} ${language}\n ${prettyCode}\n${backticks}`;
+		} catch (err) {
+			console.error("Error processing source code:", err);
+			return "";
+		}
 }
