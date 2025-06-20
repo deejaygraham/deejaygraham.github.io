96b35235f47ce587452c11641d8b93aec24a6df4
Derek Graham <deejaygraham@gmail.com>
2025-06-20 13:42:05 +0100

    Create inline-code-sample.js

---
+++ src/_11ty/shortcodes/inline-code-sample.js
@@ -0,0 +1,14 @@
+// Run prettier on an inline code sample (js) before returning
+import * as prettier from "prettier";
+
+export default function (content) {
+  const backticks = "```";
+                                  
+  const prettified = prettier.format(content, {
+    parser: "babel",
+    printWidth: 512,
+    tabWidth: 2
+  });
+                                     
+  return `${backticks} ${language}\n ${prettified}\n${backticks}`;
+}
