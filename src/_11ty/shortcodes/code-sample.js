0b641391bad2f75278c3c938052f7d50b98d2ed2
Derek Graham <deejaygraham@gmail.com>
2025-06-20 10:33:04 +0100

    Create code-sample.js

---
+++ src/_11ty/shortcodes/code-sample.js
@@ -0,0 +1,19 @@
+// Read a code sample and run prettier on the file before returning
+import * as prettier from "prettier";
+import * as fs from 'fs';
+
+export default function (language, filename) {
+  const backticks = "```";
+  const content = fs.readFileSync(filename, function(err, contents) {
+      if (err) return err;
+      return contents;
+  });
+                                  
+  const prettified = prettier.format(content, {
+    parser: language,
+    printWidth: 512,
+    tabWidth: 2
+  });
+                                     
+  return `${backticks} ${language}\n ${prettified}\n${backticks};
+}
