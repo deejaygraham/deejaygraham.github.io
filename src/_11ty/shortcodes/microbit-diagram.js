4db7fca77c1c1d825c62383b81e27ea2791f2ae2
Derek Graham <deejaygraham@gmail.com>
2025-06-25 12:45:59 +0100

    Update microbit-diagram.js

--- src/_11ty/shortcodes/microbit-diagram.js
+++ src/_11ty/shortcodes/microbit-diagram.js
@@ -9,5 +9,20 @@ const svgContent = `<svg viewBox="0 0 ${width} ${height}" xmlns="http://www.w3.o
 
 export default function () {
   // add support for display image
-  return svgContent;
+  const led_width = 5;
+  const led_height = 5;
+  const led_spacing = 50;
+
+  const displayMatrix = [];
+  
+  for (let x = 0; x < 5; x++) {
+    for (let y = 0; y < 5; y++) {
+      const led = `<rect width="${led_width}" height="${led_height}" rx="1" fill="red" />`;
+      displayMatrix.push(led);
+    }
+  }
+
+  // let content = `<svg viewBox="0 0 ${width} ${height}" xmlns="http://www.w3.org/2000/svg">`;
+  
+  return `<svg viewBox="0 0 ${width} ${height}" xmlns="http://www.w3.org/2000/svg">${microbit}</svg>`;;
 }
