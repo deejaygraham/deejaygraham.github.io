dc076a5b6d103bc87bbc2e5388f8a239eb3cceaf
Derek Graham <deejaygraham@gmail.com>
2025-06-25 14:18:28 +0100

    Update microbit-diagram.js

--- src/_11ty/shortcodes/microbit-diagram.js
+++ src/_11ty/shortcodes/microbit-diagram.js
@@ -4,27 +4,33 @@
 const width = 721;
 const height = 565;
 const corner = 15; // px
-const microbit = `<rect width="${width}" height="${height}" rx="${corner}" class="microbit-body" />`;
 
 export default function () {
   // add support for display image
   const svgBuilder = []
 
   svgBuilder.push(`<svg viewBox="0 0 ${width} ${height}" xmlns="http://www.w3.org/2000/svg">`);
+  // add styling...
   svgBuilder.push("<style>");
   svgBuilder.push(".microbit-body { fill: black; } ");
   svgBuilder.push(".microbit-led { fill: red; } ");
   svgBuilder.push(".edge-connector { fill: gold; } ");
   svgBuilder.push("</style>");
-  svgBuilder.push(microbit);
+  
+  svgBuilder.push(`<rect width="${width}" height="${height}" rx="${corner}" class="microbit-body" />`);
 
-  const led_start_x = 200;
-  const led_start_y = 200;
+  const half_width = Math.floor(width / 2);
+  const half_height = Math.floor(height / 2);
+
+  // render led matrix
   const led_width = 10;
   const led_height = 10;
   const led_spacing_x = 50;
   const led_spacing_y = 60;
   
+  const led_start_x = half_width - (2 * led_spacing_x) - Math.floor(2.5 * led_width);
+  const led_start_y = half_height - (2 * led_spacing_y) - Math.floor(2.5 * led_height);
+
   for (let row = 0; row < 5; row++) {
     for (let column = 0; column < 5; column++) {
       const x = led_start_x + (row * led_spacing_x);
