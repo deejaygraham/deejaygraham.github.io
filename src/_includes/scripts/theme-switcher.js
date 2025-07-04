72277882fc5e628968b79e1cafd6c35bb2cdb59f
Derek Graham <deejaygraham@gmail.com>
2025-07-04 15:13:01 +0100

    Update theme-switcher.js

--- src/_includes/scripts/theme-switcher.js
+++ src/_includes/scripts/theme-switcher.js
@@ -1,6 +1,7 @@
 const themePreferenceKey = "theme-preference";
 const lightThemeValue = 'light';
 const darkThemeValue = 'dark';
+const noPreferenceValue = '';
 
 const setThemePreference = (themeName) => {
     const bulmaThemeAttrName = 'data-theme';
@@ -35,6 +36,16 @@ const getThemePreference = () => {
   }
 }
 
+const setThemeButtonState = (button, active, activeClassName) = {
+    button.setAttribute('aria-pressed', active);
+
+    if (active) {
+        button.classList.add(activeClassName);
+    } else {
+        button.classList.remove(activeClassName);
+    }
+}
+
 /* eslint-disable-next-line */
 const registerThemeSwitch = () => {
 
@@ -46,39 +57,20 @@ const registerThemeSwitch = () => {
     } 
     // no preference - leave as system preference
     
-    const lightThemeId = "light-theme-toggle";
-    const lightThemeButton = document.getElementById(lightThemeId);
-    lightThemeButton.setAttribute('aria-pressed', preference === lightThemeValue);
-
     const bulmaActiveClass = 'is-active';
     
-    if (preference === lightThemeValue) {
-        lightThemeButton.classList.add(bulmaActiveClass);
-    } else {
-        lightThemeButton.classList.remove(bulmaActiveClass);
-    }
-
+    const lightThemeId = "light-theme-toggle";
+    const lightThemeButton = document.getElementById(lightThemeId);
+    setThemeButtonState(lightThemeButton, preference === lightThemeValue, bulmaActiveClass);
     lightThemeButton.addEventListener("click", () => onClick(lightThemeValue));
 
     const darkThemeId = "dark-theme-toggle";
     const darkThemeButton = document.getElementById(darkThemeId);
-    darkThemeButton.setAttribute('aria-pressed', preference === darkThemeValue);
-
-    if (preference === darkThemeValue) {
-        darkThemeButton.classList.add(bulmaActiveClass);
-    } else {
-        darkThemeButton.classList.remove(bulmaActiveClass);
-    }
-    
+    setThemeButtonState(darkThemeButton, preference === darkThemeValue, bulmaActiveClass);
     darkThemeButton.addEventListener("click", () => onClick(darkThemeValue));
 
     const systemThemeId ="system-theme-toggle";
     const systemThemeButton = document.getElementById(systemThemeId);
-
-    if (preference === '') {
-        systemThemeButton.classList.add(bulmaActiveClass);
-    } else {
-        systemThemeButton.classList.remove(bulmaActiveClass);
-    }
-    systemThemeButton.addEventListener("click", () => onClick(''));
+    setThemeButtonState(systemThemeButton, preference === noPreferenceValue, bulmaActiveClass);
+    systemThemeButton.addEventListener("click", () => onClick(noPreferenceValue));
 };
