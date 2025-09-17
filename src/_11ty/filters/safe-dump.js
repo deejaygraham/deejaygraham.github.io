ebf1d199084687c44bafc9084b13a6992e27daa4
Derek Graham <deejaygraham@gmail.com>
2025-09-17 12:49:11 +0100

    Create safe-dump.js

---
+++ src/_11ty/filters/safe-dump.js
@@ -0,0 +1,19 @@
+// safer dump filter without recursion
+// usage: {{ post.date | safedump }}
+// thanks to https://abdullahyahya.com/2022/08/11ty-ways-to-debug-data/
+const dumpWithoutCircularity = () => {
+  const seen = new WeakSet();
+  return (key, value) => {
+    if (typeof value === "object" && value !== null) {
+      if (seen.has(value)) {
+        return;
+      }  
+      seen.add(value);
+    }
+    return value;
+  };
+};
+
+export default function (obj) {
+  return JSON.stringify(obj, dumpWithoutCircularity(), 4);
+}
