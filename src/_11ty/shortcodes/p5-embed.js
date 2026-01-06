a7c890fc1b048b6e75ceb21d0aa3115cf03592a4
Derek Graham <deejaygraham@gmail.com>
2026-01-06 11:53:13 +0000

    Add horizontal rule before source details

--- src/_11ty/shortcodes/p5-embed.js
+++ src/_11ty/shortcodes/p5-embed.js
@@ -1,4 +1,4 @@
 export default function (content) {
   // needs matching p5-container on page for setup to work
-  return `<script>${content}</script><details><summary>Source</summary><pre><code>${content}</code></pre></details>`;
+  return `<script>${content}</script> <hr /> <details><summary>Source</summary><pre><code>${content}</code></pre></details>`;
 };
