c61bd44953b9defc30ef5b1824f2482919f16ca3
Derek Graham <deejaygraham@gmail.com>
2026-01-06 14:47:19 +0000

    Update youtube.js

--- src/_11ty/shortcodes/youtube.js
+++ src/_11ty/shortcodes/youtube.js
@@ -3,7 +3,7 @@ export default function (id) {
 width: 100%;
 height: 0;
 padding-bottom: 56.25%; margin-bottom: 1rem;">
-  <iframe width="560" height="315" src="https://www.youtube.com/embed/${id}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"  style="
+  <iframe title="embedded youtube video" width="560" height="315" src="https://www.youtube.com/embed/${id}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"  style="
       position: absolute;
       top: 0;
       left: 0;
