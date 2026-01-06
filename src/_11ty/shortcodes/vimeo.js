d6cc078be314f4f2a84b49656a389ad85322805e
Derek Graham <deejaygraham@gmail.com>
2026-01-06 14:47:40 +0000

    Fix iframe tag for Vimeo shortcode

--- src/_11ty/shortcodes/vimeo.js
+++ src/_11ty/shortcodes/vimeo.js
@@ -3,7 +3,7 @@ export default function (id) {
 width: 100%;
 height: 0;
 padding-bottom: 56.25%; margin-bottom: 1rem;">
-<iframe src="https://player.vimeo.com/video/${id}" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen style="
+<iframe title="embedded vimeo video" src="https://player.vimeo.com/video/${id}" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen style="
 position: absolute;
 top: 0;
 left: 0;
