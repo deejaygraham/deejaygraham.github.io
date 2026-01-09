06ef16851194cfef88f0b5dafd2684e6e81a1acd
Derek Graham <deejaygraham@gmail.com>
2026-01-09 08:52:00 +0000

    Refactor redacted.js for improved readability

--- src/_includes/code/p5js/redacted.js
+++ src/_includes/code/p5js/redacted.js
@@ -1,67 +1,69 @@
 /* eslint-disable */
-let canvasX = 800;
-let canvasY = 800;
+const corpus = `When in the Course of human events, it becomes necessary for one people to dissolve the political bands which
+have connected them with another, and to assume among the powers of the earth, the separate and equal station to which the
+Laws of Nature and of Nature's God entitle them, a decent respect to the opinions of mankind requires that they should declare
+the causes which impel them to the separation.
+
+We hold these truths to be self-evident, that all men are created equal, that they are endowed by their Creator with certain
+unalienable Rights, that among these are Life, Liberty and the pursuit of Happiness. That to secure these rights, Governments are
+instituted among Men, deriving their just powers from the consent of the governed, That whenever any Form of Government becomes
+destructive of these ends, it is the Right of the People to alter or to abolish it, and to institute new Government, laying its
+foundation on such principles and organizing its powers in such form, as to them shall seem most likely to effect their Safety
+and Happiness. Prudence, indeed, will dictate that Governments long established should not be changed for light and transient
+causes; and accordingly all experience hath shewn, that mankind are more disposed to suffer, while evils are sufferable, than
+to right themselves by abolishing the forms to which they are accustomed. But when a long train of abuses and usurpations,
+pursuing invariably the same Object evinces a design to reduce them under absolute Despotism, it is their right, it is their
+duty, to throw off such Government, and to provide new Guards for their future security. Such has been the patient sufferance
+of these Colonies; and such is now the necessity which constrains them to alter their former Systems of Government. The history
+of the present King of Great Britain is a history of repeated injuries and usurpations, all having in direct object the
+establishment of an absolute Tyranny over these States. To prove this, let Facts be submitted to a candid world.
+`
+
+const redactionThreshold = 70; // percent
+let words = '';
 
 function setup() {
-  createCanvas(canvasX, canvasY).parent('redact');
-  textFont("Courier New")
-  textSize(24);
-  noLoop();
-  noStroke();
+	createCanvas(windowWidth, windowHeight);
+	textFont("Courier New")
+	textSize(16);
+	noLoop();
+	noStroke();
+
+	words = corpus.replace(/[\n\r\t]/gm, ' ').split(' ');
 }
 
 function draw() {
-  //const sentence = "hello world, the quick brown fox jumped over the lazy dog";
-  const corpus = `When in the Course of human events, it becomes necessary for one people to dissolve the political bands which 
-have connected them with another, and to assume among the powers of the earth, the separate and equal station to which the 
-Laws of Nature and of Nature's God entitle them, a decent respect to the opinions of mankind requires that they should declare 
-the causes which impel them to the separation.
+	const canvasX = 800;
+	const canvasY = 800;
+	const leftBorder = 20;
+	const rightBorder = canvasX;
+	const topBorder = 20;
+	const bottomBorder = canvasY;
+	const wordSpacing = 10;
+	const lineSpacing = 30;
 
-We hold these truths to be self-evident, that all men are created equal, that they are endowed by their Creator with certain 
-unalienable Rights, that among these are Life, Liberty and the pursuit of Happiness. That to secure these rights, Governments are 
-instituted among Men, deriving their just powers from the consent of the governed, That whenever any Form of Government becomes 
-destructive of these ends, it is the Right of the People to alter or to abolish it, and to institute new Government, laying its 
-foundation on such principles and organizing its powers in such form, as to them shall seem most likely to effect their Safety 
-and Happiness. Prudence, indeed, will dictate that Governments long established should not be changed for light and transient 
-causes; and accordingly all experience hath shewn, that mankind are more disposed to suffer, while evils are sufferable, than 
-to right themselves by abolishing the forms to which they are accustomed. But when a long train of abuses and usurpations, 
-pursuing invariably the same Object evinces a design to reduce them under absolute Despotism, it is their right, it is their 
-duty, to throw off such Government, and to provide new Guards for their future security. Such has been the patient sufferance 
-of these Colonies; and such is now the necessity which constrains them to alter their former Systems of Government. The history 
-of the present King of Great Britain is a history of repeated injuries and usurpations, all having in direct object the 
-establishment of an absolute Tyranny over these States. To prove this, let Facts be submitted to a candid world.
-`
-  const words = corpus.split(' ');
-  const redactionThreshold = 70;
-  
-  const leftBorder = 20;
-  const rightBorder = canvasX;
-  const topBorder = 20;
-  const bottomBorder = canvasY;
-  const wordSpacing = 10;
-  const lineSpacing = 30;
-  
-  let x = leftBorder;
-  let y = topBorder;
+	let x = leftBorder;
+	let y = topBorder;
+	
+	for (let i = 0; i < words.length; ++i) {
+		let redactWord = random(0, 100);
 
-  for (let i = 0; i < words.length; ++i) {
-    let redact = random(0, 100);
-    
-    const word = words[i].trim();
-    let w = textWidth(word);
-    if ((x + w) > canvasX) {
-      x = leftBorder;
-      y += lineSpacing;
-    }
-    
-    if (redact >= redactionThreshold) {
-      fill(51);
-      rect(x, y - (lineSpacing / 2), w, lineSpacing);
-    } else {
-      //console.log(word)
-      text(word, x, y);
-    }
-    
-    x += wordSpacing + w;
-  }
+		const word = words[i].trim();
+		const wordWidth = textWidth(word);
+		
+		if ((x + wordWidth) > canvasX) {
+			// reset to new line
+			x = leftBorder;
+			y += lineSpacing;
+		}
+
+		if (redactWord >= redactionThreshold) {
+			fill(51);
+			rect(x, y - (lineSpacing / 2), wordWidth, lineSpacing);
+		} else {
+			text(word, x, y);
+		}
+
+		x += wordSpacing + wordWidth;
+	}
 }
