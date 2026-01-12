342ed6a987c3c42259494a0356ba3658c1086bec
Derek Graham <deejaygraham@gmail.com>
2026-01-12 11:19:34 +0000

    Update p5-embed.js

--- src/_11ty/shortcodes/p5-embed.js
+++ src/_11ty/shortcodes/p5-embed.js
@@ -1,4 +1,5 @@
-export default function (filename) {
-  // needs matching p5-container on page for setup to work
-  return `<script src="/js/${filename}"></script>`;
+export default function (content, id, filename) {
+  const container = `<div id="${id}" class="p5-container" data-prefix="p5-sketch"></div>`;
+  const scriptLink = `<script src="/js/${filename}"></script>`;
+  return container + '\n' + scriptLink + '\n' + content; // `<details><summary>Source</summary>${content}</details>`;
 };
