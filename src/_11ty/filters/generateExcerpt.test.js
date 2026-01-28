0026d56f5ecff3a63c72c7c70b7499c4b949ab4a
Derek Graham <deejaygraham@gmail.com>
2026-01-28 11:52:54 +0000

    Add tests for generateExcerpt function

--- src/_11ty/filters/generateExcerpt.test.js
+++ src/_11ty/filters/generateExcerpt.test.js
@@ -25,3 +25,18 @@ test("long text is truncated at space closest to limit", (t) => {
   const content = 'This is some longer text';
   t.is('This is some', generateExcerpt(content, 15));
 });
+
+test("shortcodes are removed", (t) => {
+  const content = 'This is a youtube video {% youtube "12345" %}';
+  t.is('This is a youtube video', generateExcerpt(content, 100));
+});
+
+test("title hashes are removed", (t) => {
+  const content = 'This is a blog post ## Sub heading goes here';
+  t.is('This is a blog post', generateExcerpt(content, 100));
+});
+
+test("code fences are removed", (t) => {
+  const content = 'This is some awesome code ```python i = 1 ```';
+  t.is('This is some awesome code', generateExcerpt(content, 100));
+});
