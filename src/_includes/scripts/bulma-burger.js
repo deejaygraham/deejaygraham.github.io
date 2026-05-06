1ac7771d61f2ea5ce763082c36889a676af52b0e
Derek Graham <deejaygraham@gmail.com>
2026-05-06 14:02:21 +0100

    Update bulma-burger.js

--- src/_includes/scripts/bulma-burger.js
+++ src/_includes/scripts/bulma-burger.js
@@ -1,9 +1,16 @@
 // The following code is based off a toggle menu by @Bradcomp
 // source: https://gist.github.com/Bradcomp/a9ef2ef322a8e8017443b626208999c1
-/* eslint-disable-next-line */
-const addBulmaBurgerMenu = () => {
+export const addBulmaBurgerMenu = () => {
     var burger = document.querySelector('.navbar-burger');
+    if (!burger) {
+        return;
+    }
+
     var menu = document.querySelector('#' + burger.dataset.target);
+    if (!menu) {
+        return;
+    }
+
     burger.addEventListener('click', function () {
         burger.classList.toggle('is-active');
         menu.classList.toggle('is-active');
