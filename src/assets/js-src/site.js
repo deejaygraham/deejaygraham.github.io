7bde906537cbca698a08776f5fb4170bd506e051
Derek Graham <deejaygraham@gmail.com>
2026-05-06 14:14:14 +0100

    Add site.js to manage site functionalities
    Initialize site.js with various utility functions for the site.

---
+++ src/assets/js-src/site.js
@@ -0,0 +1,24 @@
+import { addBulmaBurgerMenu } from "../../_includes/scripts/bulma-burger.js";
+import { registerServiceWorker } from "../../_includes/scripts/register-service-worker.js";
+import { registerThemeSwitch } from "../../_includes/scripts/theme-switcher.js";
+import { addCopyButtonToSourceCode } from "../../_includes/scripts/add-source-copy-button.js";
+import { renderMicrobits } from "../../_includes/scripts/microbit.js";
+import { initNarratePostContent } from "../../_includes/scripts/read-content-aloud.js";
+
+const runWhenDocumentReady = (callback) => {
+  if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", callback, { once: true });
+    return;
+  }
+
+  callback();
+};
+
+runWhenDocumentReady(() => {
+  registerServiceWorker();
+  addBulmaBurgerMenu();
+  registerThemeSwitch();
+  addCopyButtonToSourceCode();
+  renderMicrobits();
+  initNarratePostContent();
+});
