30e8f052533dfc069c437341489a384870be3214
Derek Graham <deejaygraham@gmail.com>
2026-06-26 10:49:18 +0100

    Add paths for social preview image caching

---
+++ src/_11ty/plugins/generate-social-images/paths.js
@@ -0,0 +1,5 @@
+/** Persisted between builds; restored from GitHub Actions cache on CI. */
+export const SOCIAL_PREVIEW_CACHE_DIR = "build-cache/img/previews";
+
+/** Published under /img/previews/ on the deployed site. */
+export const SOCIAL_PREVIEW_SITE_DIR = "_site/img/previews";
