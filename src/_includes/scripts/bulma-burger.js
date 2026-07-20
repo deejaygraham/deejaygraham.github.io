2d12cb91bd9d901239e704cb6c8ed59e35a47376
Derek Graham <deejaygraham@gmail.com>
2026-07-20 08:39:13 +0100

    Remove Bulma burger menu implementation
    Remove the implementation of the Bulma burger menu as it is deprecated in favor of DaisyUI dropdown.

--- src/_includes/scripts/bulma-burger.js
+++ src/_includes/scripts/bulma-burger.js
@@ -1,18 +1,2 @@
-// The following code is based off a toggle menu by @Bradcomp
-// source: https://gist.github.com/Bradcomp/a9ef2ef322a8e8017443b626208999c1
-export const addBulmaBurgerMenu = () => {
-    var burger = document.querySelector('.navbar-burger');
-    if (!burger) {
-        return;
-    }
-
-    var menu = document.querySelector('#' + burger.dataset.target);
-    if (!menu) {
-        return;
-    }
-
-    burger.addEventListener('click', function () {
-        burger.classList.toggle('is-active');
-        menu.classList.toggle('is-active');
-    });
-};
+// Deprecated: mobile nav uses DaisyUI dropdown in siteheader.njk (no JS toggle).
+export const addBulmaBurgerMenu = () => {};
