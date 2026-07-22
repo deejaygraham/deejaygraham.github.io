108c2db97cca5dc63e0c21e678998d65d72835a5
Derek Graham <deejaygraham@gmail.com>
2026-07-22 21:23:13 +0100

    Update about.spec.js

--- tests/about.spec.js
+++ tests/about.spec.js
@@ -6,7 +6,7 @@ test.describe("about page", () => {
   test("renders correctly", async ({ page }) => {
     await page.goto("/about/");
 
-    await expect(page).toHaveTitle(/Hey, Hi, Hello./);
+    await expect(page).toHaveTitle(/hey, hi, hello./);
     await expect(page.getByText("My name is Derek Graham and this is my personal blog.")).toBeVisible();
   });
 
