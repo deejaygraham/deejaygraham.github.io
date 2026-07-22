c2633f64b91def467ad97d40b0484c18fa9da6cb
Derek Graham <deejaygraham@gmail.com>
2026-07-22 15:10:09 +0100

    Add test for collapsing whitespace in HTML excerpts
    Added a test to ensure whitespace and newlines are collapsed in HTML content.

--- src/_11ty/filters/excerpt.test.js
+++ src/_11ty/filters/excerpt.test.js
@@ -14,3 +14,10 @@ test("long post is truncated with elipsis", (t) => {
   const longPost = "This is a longer post ".repeat(10);
   t.regex(excerpt(longPost), /post This is a longer post...$/);
 });
+
+test("collapses whitespace and newlines from html content", (t) => {
+  const messy = "<p>Hello</p>\n\n    <p>world   from\n\ttags</p>";
+  const result = excerpt(messy);
+  t.true(result.startsWith("Hello world from"));
+  t.false(/\s{2,}|\n|\t/.test(result));
+});
