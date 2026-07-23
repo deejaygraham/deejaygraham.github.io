df4f0e08d73ab455dbcbc444c80be0efc5ba5f5c
Derek Graham <deejaygraham@gmail.com>
2026-07-23 15:08:10 +0100

    Add default social preview image constants

--- src/_11ty/plugins/generate-social-images/paths.js
+++ src/_11ty/plugins/generate-social-images/paths.js
@@ -3,3 +3,7 @@ export const SOCIAL_PREVIEW_CACHE_DIR = "build-cache/img/previews";
 
 /** Published under /img/previews/ on the deployed site. */
 export const SOCIAL_PREVIEW_SITE_DIR = "_site/img/previews";
+
+/** Shared OG image for non-post pages (about, index, archive, …). */
+export const SOCIAL_PREVIEW_DEFAULT_SLUG = "default";
+export const SOCIAL_PREVIEW_DEFAULT_FILE = `${SOCIAL_PREVIEW_DEFAULT_SLUG}.jpg`;
