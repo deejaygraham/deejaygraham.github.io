081808d1bfbec7507cb39347f3e8ef92fdd0ce0f
Derek Graham <deejaygraham@gmail.com>
2026-07-23 15:06:01 +0100

    Enhance tests for socialImageFile functionality
    Added tests for socialImageFile functions including slugification, blog post detection, and OG image filename generation.

--- src/_11ty/filters/socialImageFile.test.js
+++ src/_11ty/filters/socialImageFile.test.js
@@ -1,7 +1,30 @@
 import test from "ava";
-import socialImageFile from "./socialImageFile.js";
+import socialImageFile, {
+  isBlogPostInputPath,
+  ogImageFile,
+} from "./socialImageFile.js";
 
 test("returns slugified jpg filename", (t) => {
   t.is(socialImageFile("aggressive-dates"), "aggressive-dates.jpg");
   t.is(socialImageFile("Hello World"), "hello-world.jpg");
+});
+
+test("detects blog post input paths", (t) => {
+  t.true(
+    isBlogPostInputPath("./src/content/posts/2026-07-13-aggressive-dates.md"),
+  );
+  t.true(
+    isBlogPostInputPath("src\\content\\posts\\2026-07-13-aggressive-dates.md"),
+  );
+  t.false(isBlogPostInputPath("./src/content/about.md"));
+  t.false(isBlogPostInputPath("./src/content/index.njk"));
+});
+
+test("ogImageFile uses per-post slug for posts and default otherwise", (t) => {
+  t.is(
+    ogImageFile("./src/content/posts/2026-07-13-aggressive-dates.md", "aggressive-dates"),
+    "aggressive-dates.jpg",
+  );
+  t.is(ogImageFile("./src/content/about.md", "about"), "default.jpg");
+  t.is(ogImageFile("./src/content/index.njk", ""), "default.jpg");
 });
