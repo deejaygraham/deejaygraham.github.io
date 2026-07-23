4b0fbb2f6f339a9b95d9313438d0d014548b7a90
Derek Graham <deejaygraham@gmail.com>
2026-07-23 14:43:24 +0100

    Update apply-plugins.js

--- src/_11ty/config/apply-plugins.js
+++ src/_11ty/config/apply-plugins.js
@@ -3,12 +3,10 @@ import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 import favicons from "eleventy-plugin-gen-favicons";
 
 import syntaxHighlighter from "../plugins/shiki-syntax-highlighter/index.js";
-import socialImages from "../plugins/generate-social-images/index.js";
 
 export default function applyPlugins(eleventyConfig) {
   eleventyConfig.addPlugin(syntaxHighlighter);
   eleventyConfig.addPlugin(IdAttributePlugin);
-  eleventyConfig.addPlugin(socialImages, { siteName: "d.j. graham" });
   eleventyConfig.addPlugin(favicons);
   eleventyConfig.addPlugin(InputPathToUrlTransformPlugin);
   eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
