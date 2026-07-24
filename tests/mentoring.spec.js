585938b86cae5cc8663007e358e877ab28c89adf
Derek Graham <deejaygraham@gmail.com>
2026-07-24 13:43:57 +0100

    Skip rendering test and uncomment visibility checks

--- tests/mentoring.spec.js
+++ tests/mentoring.spec.js
@@ -3,12 +3,12 @@ import { test, expect } from '@playwright/test';
 import checkPageLinksExist from './util/check-page-links-exist.js';
 
 test.describe("mentoring page", () => {
-  test("renders correctly", async ({ page }) => {
+  test.skip("renders correctly", async ({ page }) => {
     await page.goto("/mentoring/");
 
-    //await expect(page.getByRole('heading', { name: "We Think Code" })).toBeVisible();
-    //await expect(page.getByRole('heading', { name: "My Mentoring Philosophy" })).toBeVisible();
-    //await expect(page.getByRole('heading', { name: "What I Can Help With" })).toBeVisible();
+    await expect(page.getByRole('heading', { name: "We Think Code" })).toBeVisible();
+    await expect(page.getByRole('heading', { name: "My Mentoring Philosophy" })).toBeVisible();
+    await expect(page.getByRole('heading', { name: "What I Can Help With" })).toBeVisible();
   });
 
   test('Page links are correct', async ({page}) => {
