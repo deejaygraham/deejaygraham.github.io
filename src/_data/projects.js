3c5b91089c2090de2c907b4066ebb73f5719f6a3
Derek Graham <deejaygraham@gmail.com>
2026-08-03 12:55:20 +0100

    Update projects.js

--- src/_data/projects.js
+++ src/_data/projects.js
@@ -13,42 +13,42 @@ export default [
       title: "NDifference",
       subtitle: "My .Net API reporting tool",
     },
-    url: `{site.url}/ndifference/`
+    url: `${site.url}/ndifference/`
   },
   {
     data: {
       title: "Simple Microbit Assert",
       subtitle: "Minimal python unit test assertion library for the microbit"
     },
-    url: `{site.urls.github}/simple-microbit-assert`
+    url: `${site.urls.github}/simple-microbit-assert`
   },
   {
     data: {
       title: "Makers and Creators project resources",
       subtitle: "Making computers fun for 8-18 year olds",
     },
-    url:  `{site.url}/makers-n-creators/`,
+    url:  `${site.url}/makers-n-creators/`,
   },
   {
     data: {
       title: "Scrum Planning Poker",
       subtitle: "Cards to use when estimating in the planning game",
     },
-    url: `{site.urls.github}/PlanningPoker/`,
+    url: `${site.urls.github}/PlanningPoker/`,
   },
   {
     data: {
         title: "littleTiger",
         subtitle: "My first ever personal site when I was first learning about html and css via dialup"
     },
-    url: `{site.urls.github}/littleTiger`,
+    url: `${site.urls.github}/littleTiger`,
   },
   {
     data: {
       title: "OMSPA",
       subtitle: "My port of Peter Coad's companion website for the strategies and patterns handbook"
     },
-    url: `{site.urls.github}/omspa/`,
+    url: `${site.urls.github}/omspa/`,
   },
   {
     data: {
