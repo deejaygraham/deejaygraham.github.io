c30b3ad39c853a6b17a787988b1db74b87be4a3a
Derek Graham <deejaygraham@gmail.com>
2026-08-10 14:10:05 +0100

    Import expect from Playwright test module

--- tests/deployment.spec.js
+++ tests/deployment.spec.js
@@ -1,5 +1,5 @@
 // @ts-check
-import { test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import checkAllResourcesExist from './util/check-all-resources-exist.js';
 import checkSocialPreview from './util/check-social-preview.js';
 
